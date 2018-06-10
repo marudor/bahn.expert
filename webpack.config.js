830ca25dd8d916350db7182e0be8148cf667afb7
marudor <marudor@marudor.de>
2018-06-10 16:04:05 +0200

    CSS minify

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    },
+  }),
   new HtmlWebpackPlugin({
     template: path.resolve(__dirname, 'src/index.html'),
     minifiy: !isDev,
   }),
 ];
 
+const optimization = {};
+
 const rules = [
   {
     test: /\.jsx?$/,
-    use: ['cache-loader', 'babel-loader'],
+    use: ['babel-loader'],
   },
   {
     test: /\.s?css$/,
     use: [
-      { loader: 'cache-loader' },
       {
-        loader: 'style-loader',
+        loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       },
       { loader: 'css-loader' },
       { loader: 'postcss-loader' },
@@ -35,6 +44,26 @@ if (isDev) {
   plugins.push(...[new webpack.HotModuleReplacementPlugin()]);
 
   rules.forEach(r => r.use.unshift({ loader: 'cache-loader' }));
+} else {
+  plugins.push(
+    ...[
+      new MiniCssExtractPlugin({
+        filename: '[name]-[hash].css',
+        chunkFilename: '[id]-[hash].css',
+      }),
+    ]
+  );
+  optimization.minimizer = [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      extractComments: true,
+    }),
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+    }),
+  ];
 }
 
 module.exports = {
@@ -48,6 +77,11 @@ module.exports = {
     alias: {
       'lodash-es': 'lodash',
     },
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name]-[hash].js',
+    publicPath: '/',
   },
   module: {
     rules,
