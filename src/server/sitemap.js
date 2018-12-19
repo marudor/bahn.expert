620694a968c6067e301d623392da35bfceb46640
marudor <marudor@marudor.de>
2018-12-19 15:30:57 +0100

    fix sitemap

--- src/server/sitemap.js
+++ src/server/sitemap.js
@@ -5,7 +5,7 @@ export default () => {
   let xml = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
   rawStations.forEach(s => {
-    xml += `<url><loc>https://marudor.de/${s.name}</loc></url>`;
+    xml += `<url><loc>https://marudor.de/${encodeURIComponent(s.name)}</loc></url>`;
   });
 
   xml += '</urlset>';
