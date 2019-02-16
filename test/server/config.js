b1a6facd1bc684cd728421f807961a5b989a90ed
marudor <marudor@marudor.de>
2019-02-17 00:14:57 +0100

    assume prod for tests

--- test/server/config.js
+++ test/server/config.js
@@ -3,6 +3,8 @@ import '@babel/polyfill';
 import 'server/localStorageShim';
 import Nock from 'nock';
 
+global.PROD = true;
+
 beforeAll(() => {
   Nock.disableNetConnect();
   Nock.enableNetConnect(/127\.0\.0\.1/);
