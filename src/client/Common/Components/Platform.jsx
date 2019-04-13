0a122699ebdc606383a39c4fffa579cbea94820d
marudor <marudor@marudor.de>
2019-04-14 01:06:32 +0200

    Small routing bugfix

--- src/client/Common/Components/Platform.jsx
+++ src/client/Common/Components/Platform.jsx
@@ -5,15 +5,16 @@ import React from 'react';
 import withStyles, { type StyledProps } from 'react-jss';
 
 type OwnProps = {|
+  className?: string,
   cancelled?: boolean,
   scheduled?: string,
   real?: string,
 |};
 type Props = StyledProps<OwnProps, typeof styles>;
 
-const Platform = ({ classes, cancelled, scheduled, real }) => (
+const Platform = ({ classes, className, cancelled, scheduled, real }) => (
   <span
-    className={cc({
+    className={cc(className, {
       [classes.cancelled]: cancelled,
       [classes.delayed]: scheduled && scheduled !== real,
     })}
