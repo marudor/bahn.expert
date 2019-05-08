ebb09cd477dbd0e0b5c608016c50a41994f00435
marudor <marudor@marudor.de>
2019-05-09 01:59:29 +0200

    respect prefer-color-scheme

--- src/client/Common/actions/config.ts
+++ src/client/Common/actions/config.ts
@@ -16,15 +16,23 @@ const Actions = {
 
 export default Actions;
 
+// eslint-disable-next-line no-nested-ternary
+const defaultTheme = global.SERVER
+  ? ThemeType.light
+  : window.matchMedia('(prefers-color-scheme: dark)').matches
+  ? ThemeType.dark
+  : ThemeType.light;
+
 export const setCookies = (cookies: Cookies): CommonThunkResult => (
   dispatch,
   getState
 ) => {
   const darkModeFeature = getState().features.dark;
   const theme = darkModeFeature
-    ? (ThemeType[cookies.get('theme')] as undefined | ThemeType) ||
-      ThemeType.light
+    ? (ThemeType[cookies.get('theme')] as undefined | ThemeType) || defaultTheme
     : ThemeType.light;
+
+  cookies.set('theme', theme, setCookieOptions);
 
   dispatch(Actions.setTheme(theme));
   dispatch(Actions.setCookies(cookies));
