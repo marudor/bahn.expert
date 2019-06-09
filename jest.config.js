9d48e7f948fa8ecc74176ba405fa56d78db78d78
marudor <marudor@marudor.de>
2019-06-09 13:07:29 +0200

    Test structure change

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projects: ['test/client/jest.config.js', 'test/server/jest.config.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageReporters: ['text-summary', 'lcov'],
+  coverageDirectory: '<rootDir>/reports',
+  reporters: ['default', 'jest-junit'],
+};
--- test/server/jest.config.js
+++ test/server/jest.config.js
@@ -5,15 +5,6 @@ module.exports = {
     '\\.(scss|css)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/test/server/config.ts'],
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/server/**/*.{ts,tsx}'],
-  coveragePathIgnorePatterns: [
-    '<rootDir>/src/server/testData/',
-    '<rootDir>/src/server/index.ts',
-  ],
-  coverageReporters: ['text-summary', 'lcov'],
-  coverageDirectory: '<rootDir>/reports/server',
   rootDir: '../..',
   roots: ['<rootDir>/src', '<rootDir>/test/server'],
-  verbose: false,
 };
