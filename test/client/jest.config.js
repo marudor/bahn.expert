df0c55c43d6d489ec9ad17e7e70bf7c44ce78962
marudor <marudor@marudor.de>
2019-06-09 13:07:29 +0200

    Test structure change

--- test/client/jest.config.js
+++ test/client/jest.config.js
@@ -5,11 +5,6 @@ module.exports = {
     '\\.(scss|css)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/test/client/config.ts'],
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/client/**/*.{ts,tsx}'],
-  coverageReporters: ['text-summary', 'lcov'],
-  coverageDirectory: '<rootDir>/reports/client',
   rootDir: '../..',
   roots: ['<rootDir>/src', '<rootDir>/test/client'],
-  verbose: false,
 };
