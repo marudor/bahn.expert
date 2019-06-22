d9777fd48881ede11233476b0dc0f4e461de124d
marudor <marudor@marudor.de>
2019-06-23 00:01:10 +0200

    Fix tests after openDB removal

--- test/server/Search/Favendo.test.ts
+++ test/server/Search/Favendo.test.ts
@@ -1,6 +1,4 @@
-import { favendoOpenDBCombined } from 'server/Search';
 import { testHamburgSearch } from './common';
-import exampleOpenDBRespone from './__fixtures__/OpenDB-forCombine.example';
 import exampleRespone from './__fixtures__/Favendo.example';
 import FavendoSearch from 'server/Search/Favendo';
 import Nock from 'nock';
@@ -17,33 +15,6 @@ describe('Favendo Search', () => {
     await testHamburgSearch(FavendoSearch, {
       includeFavendoId: true,
     });
-  });
-
-  it('Correct combined Favendo & openDB', async () => {
-    Nock('https://si.favendo.de')
-      .get('/station-info/rest/api/search')
-      .query({
-        searchTerm: 'Hamburg',
-      })
-      .reply(200, exampleRespone);
-
-    Nock('https://open-api.bahn.de')
-      .get('/bin/rest.exe/location.name')
-      .query((q: any) => {
-        expect(q.input).toBe('Hamburg');
-        expect(q.format).toBe('json');
-
-        return true;
-      })
-      .reply(200, exampleOpenDBRespone);
-
-    const result = await favendoOpenDBCombined('Hamburg');
-
-    expect(result.map(r => r.title)).toEqual([
-      'Hamburg Hbf',
-      'Hamburg Dammtor',
-      'Hamburg Dammtor-test',
-    ]);
   });
 
   it('Throws exception on error', async () => {
