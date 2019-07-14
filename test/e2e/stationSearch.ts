4e3885aedba2b17634fbcad4b41f69c679801a16
marudor <marudor@marudor.de>
2019-07-14 15:54:56 +0200

    Allow clearing of stationSearch. closes #23

---
+++ test/e2e/stationSearch.ts
@@ -0,0 +1,23 @@
+import { Selector } from 'testcafe';
+
+fixture`Station Search`.page(process.env.E2E_URL || '');
+
+test('Clearable station', async t => {
+  const inputSelector = '[data-testid="stationSearch"] input';
+  const singleValueSelector = '[data-testid="singleValue"]';
+  const placeholderSelector = '[data-testid="placeholder"]';
+
+  await t.typeText(inputSelector, 'Kiel Hbf');
+  const firstResult = Selector('[data-testid="menuItem"]');
+
+  await t.expect(firstResult.textContent).eql('Kiel Hbf');
+  await t.click(firstResult);
+  await t.expect(Selector(singleValueSelector).textContent).eql('Kiel Hbf');
+  await t.expect(Selector(placeholderSelector).exists).notOk();
+  await t
+    .click(inputSelector)
+    .pressKey('backspace')
+    .expect(Selector(placeholderSelector).exists)
+    .ok();
+  await t.expect(Selector(singleValueSelector).exists).notOk();
+});
