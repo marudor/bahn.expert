262bd97532f3d2631bd80537a2a6e238c0d321c2
marudor <marudor@marudor.de>
2019-07-18 11:52:39 +0200

    Dependencies

--- src/client/Abfahrten/Components/MostUsed.tsx
+++ src/client/Abfahrten/Components/MostUsed.tsx
@@ -2,21 +2,21 @@ import FavEntry from './FavEntry';
 import React from 'react';
 
 const mostUsed = [
-  { title: 'Hannover Hbf', id: '8000152' },
-  { title: 'Köln Hbf', id: '8000207' },
-  { title: 'Düsseldorf Hbf', id: '8000085' },
-  { title: 'Wuppertal Hbf', id: '8000266' },
-  { title: 'Mannheim Hbf', id: '8000244' },
   { title: 'Frankfurt (Main) Hbf', id: '8098105' },
-  { title: 'Hamburg Hbf', id: '8002549' },
-  { title: 'Chemnitz Hbf', id: '8010184' },
-  { title: 'Berlin Hauptbahnhof', id: '8011160' },
-  { title: 'Rheda-Wiedenbrück', id: '8000315' },
-  { title: 'Kempen (Niederrhein)', id: '8000409' },
+  { title: 'Hannover Hbf', id: '8000152' },
   { title: 'Stuttgart Hbf', id: '8000096' },
+  { title: 'Köln Hbf', id: '8000207' },
+  { title: 'Hamburg Hbf', id: '8002549' },
+  { title: 'Berlin Hauptbahnhof', id: '8011160' },
+  { title: 'Düsseldorf Hbf', id: '8000085' },
+  { title: 'Mannheim Hbf', id: '8000244' },
+  { title: 'Villingen (Schwarzw)', id: '8000366' },
+  { title: 'Karlsruhe Hbf', id: '8000191' },
+  { title: 'Wuppertal Hbf', id: '8000266' },
+  { title: 'Nürnberg Hbf', id: '8000284' },
+  { title: 'München Hbf', id: '8000261' },
   { title: 'Bonn Hbf', id: '8000044' },
-  { title: 'Opladen', id: '8000853' },
-  { title: 'Berlin-Spandau', id: '8010404' },
+  { title: 'Leipzig Hbf', id: '8098205' },
 ];
 
 // eslint-disable-next-line react/display-name
