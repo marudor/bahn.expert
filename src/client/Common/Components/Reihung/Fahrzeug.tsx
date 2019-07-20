dc36b51e4b158f9ad0f1927aa27a310be4827105
marudor <marudor@marudor.de>
2019-07-20 11:37:03 +0200

    Use update of WR API

--- src/client/Common/Components/Reihung/Fahrzeug.tsx
+++ src/client/Common/Components/Reihung/Fahrzeug.tsx
@@ -5,7 +5,7 @@ import cc from 'clsx';
 import ChildFriendly from '@material-ui/icons/ChildFriendly';
 import MapsLocalDining from '@material-ui/icons/LocalDining';
 import NotificationsOff from '@material-ui/icons/NotificationsOff';
-import React, { useMemo } from 'react';
+import React from 'react';
 import useStyles from './Fahrzeug.style';
 import WagenLink from './WagenLink';
 
@@ -19,92 +19,18 @@ export type OwnProps = InheritedProps & {
   fahrzeug: Fahrzeug;
   destination?: string;
   wrongWing: boolean;
-  comfort?: boolean;
-  quiet?: boolean;
-  toddler?: boolean;
 };
 
 type Props = OwnProps;
-
-// Klasse: 0 = unknown
-// Klasse: 1 = Nur erste
-// Klasse: 2 = Nur zweite
-// Klasse: 3 = 1 & 2
-// klasse: 4 = Nicht für Passagiere. z.B. Triebkopf
-type AdditionalFahrzeugInfos = {
-  klasse: 0 | 1 | 2 | 3 | 4;
-  speise: boolean;
-  rollstuhl: boolean;
-  fahrrad: boolean;
-};
-
-function getFahrzeugInfo(fahrzeug: Fahrzeug): AdditionalFahrzeugInfos {
-  const data: AdditionalFahrzeugInfos = {
-    klasse: 0,
-    speise: Boolean(
-      fahrzeug.allFahrzeugausstattung.find(a => a.ausstattungsart === 'BISTRO')
-    ),
-    rollstuhl: Boolean(
-      fahrzeug.allFahrzeugausstattung.find(
-        a => a.ausstattungsart === 'PLAETZEROLLSTUHL'
-      )
-    ),
-    fahrrad: Boolean(
-      fahrzeug.allFahrzeugausstattung.find(
-        a => a.ausstattungsart === 'PLAETZEFAHRRAD'
-      )
-    ),
-  };
-
-  switch (fahrzeug.kategorie) {
-    case 'DOPPELSTOCKSTEUERWAGENZWEITEKLASSE':
-    case 'DOPPELSTOCKWAGENZWEITEKLASSE':
-    case 'REISEZUGWAGENZWEITEKLASSE':
-    case 'STEUERWAGENZWEITEKLASSE':
-      data.klasse = 2;
-      break;
-    case 'HALBSPEISEWAGENZWEITEKLASSE':
-    case 'SPEISEWAGEN':
-      data.klasse = 2;
-      data.speise = true;
-      break;
-    default:
-      break;
-    case 'DOPPELSTOCKWAGENERSTEZWEITEKLASSE':
-    case 'DOPPELSTOCKSTEUERWAGENERSTEZWEITEKLASSE':
-    case 'STEUERWAGENERSTEZWEITEKLASSE':
-    case 'REISEZUGWAGENERSTEZWEITEKLASSE':
-      data.klasse = 3;
-      break;
-    case 'HALBSPEISEWAGENERSTEKLASSE':
-      data.klasse = 1;
-      data.speise = true;
-      break;
-    case 'DOPPELSTOCKWAGENERSTEKLASSE':
-    case 'REISEZUGWAGENERSTEKLASSE':
-    case 'STEUERWAGENERSTEKLASSE':
-      data.klasse = 1;
-      break;
-    case 'TRIEBKOPF':
-    case 'LOK':
-      data.klasse = 4;
-  }
-
-  return data;
-}
 
 const FahrzeugComp = ({
   fahrzeug,
   wrongWing,
   scale,
   correctLeft,
-  comfort,
-  quiet,
-  toddler,
   type,
 }: Props) => {
   const classes = useStyles();
-  const info = useMemo(() => getFahrzeugInfo(fahrzeug), [fahrzeug]);
 
   const { startprozent, endeprozent } = fahrzeug.positionamhalt;
   const start = Number.parseInt(startprozent, 10);
@@ -116,7 +42,7 @@ const FahrzeugComp = ({
   };
 
   // @ts-ignore
-  const klasseClassName: keyof typeof classes = `klasse${info.klasse}`;
+  const klasseClassName: keyof typeof classes = `klasse${fahrzeug.additionalInfo.klasse}`;
 
   return (
     <div
@@ -129,13 +55,23 @@ const FahrzeugComp = ({
       <span className={cc(classes.klasse, classes[klasseClassName])} />
       <span className={classes.nummer}>{fahrzeug.wagenordnungsnummer}</span>
       <span className={classes.icons}>
-        {info.rollstuhl && <ActionAccessible className={classes.icon} />}
-        {info.fahrrad && <ActionMotorcycle className={classes.icon} />}
-        {info.speise && <MapsLocalDining className={classes.icon} />}
-        {quiet && <NotificationsOff className={classes.icon} />}
-        {toddler && <ChildFriendly className={classes.icon} />}
+        {fahrzeug.additionalInfo.rollstuhl && (
+          <ActionAccessible className={classes.icon} />
+        )}
+        {fahrzeug.additionalInfo.fahrrad && (
+          <ActionMotorcycle className={classes.icon} />
+        )}
+        {fahrzeug.additionalInfo.speise && (
+          <MapsLocalDining className={classes.icon} />
+        )}
+        {fahrzeug.additionalInfo.ruhe && (
+          <NotificationsOff className={classes.icon} />
+        )}
+        {fahrzeug.additionalInfo.kleinkind && (
+          <ChildFriendly className={classes.icon} />
+        )}
       </span>
-      {comfort && <span className={classes.comfort} />}
+      {fahrzeug.additionalInfo.comfort && <span className={classes.comfort} />}
       <WagenLink
         type={type}
         fahrzeugnummer={fahrzeug.fahrzeugnummer}
