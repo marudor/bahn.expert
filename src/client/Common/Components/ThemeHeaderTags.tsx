f12b2bffc4daf71292617c0c30cf24ecf57e5ece
marudor <marudor@marudor.de>
2019-07-22 18:23:54 +0200

    Correctly dynamically set theme-color

---
+++ src/client/Common/Components/ThemeHeaderTags.tsx
@@ -0,0 +1,20 @@
+import { MergedTheme, useTheme } from '@material-ui/styles';
+import Helmet from 'react-helmet';
+import React from 'react';
+
+const ThemeHeaderTags = () => {
+  const theme = useTheme<MergedTheme>();
+
+  return (
+    <Helmet>
+      <meta name="theme-color" content={theme.palette.background.default} />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+    </Helmet>
+  );
+};
+
+export default ThemeHeaderTags;
