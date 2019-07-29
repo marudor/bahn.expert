693684ad4cbfd074029d0f498eb55c05ff162be9
marudor <marudor@marudor.de>
2019-07-29 17:37:08 +0200

    Last component from class to hooks

---
+++ src/client/Abfahrten/Components/AbfahrtenList.style.ts
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/styles';
+
+export default makeStyles(theme => ({
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.shape.headerSpacing,
+  },
+  lookaheadMarker: {
+    height: 154,
+    position: 'absolute',
+    bottom: 0,
+  },
+
+  lookahead: {},
+
+  lookbehind: {
+    position: 'relative',
+    paddingTop: 10,
+    backgroundColor: theme.colors.shadedBackground,
+  },
+}));
