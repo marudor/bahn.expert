7440a4e451ddeec0eca8dbc89f29747ddbbc7114
marudor <marudor@marudor.de>
2019-08-07 15:56:55 +0200

    Add onlyDepartures, fixes #31

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,11 +1,22 @@
 import '@testing-library/cypress/add-commands';
 
-Cypress.Commands.add('navigateToStation', (value: string) => {
-  cy.server();
-  cy.route(/\/api\/iris\/current\/abfahrten.*/).as('irisAbfahrten');
-  cy.getByTestId('stationSearchInput').type(value);
-  cy.getAllByTestId('stationSearchMenuItem')
-    .first()
-    .click();
-  cy.wait('@irisAbfahrten');
+Cypress.Commands.add(
+  'navigateToStation',
+  (value: string, addRoute: boolean = true) => {
+    cy.server();
+    if (addRoute) {
+      cy.route(/\/api\/iris\/current\/abfahrten.*/).as('irisAbfahrten');
+    }
+    cy.getByTestId('stationSearchInput').type(value);
+    cy.getAllByTestId('stationSearchMenuItem')
+      .first()
+      .click();
+    if (addRoute) {
+      cy.wait('@irisAbfahrten');
+    }
+  }
+);
+
+Cypress.Commands.add('closeModal', () => {
+  cy.get('.MuiBackdrop-root').click({ force: true });
 });
