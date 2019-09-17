1399132bf6542545a6990aa3952536b9f3a7b350
marudor <marudor@marudor.de>
2019-09-18 00:42:52 +0200

    Add insa

--- src/server/HAFAS/Request.ts
+++ src/server/HAFAS/Request.ts
@@ -1,3 +1,4 @@
+import * as HafasProfiles from './profiles';
 import {
   AllowedHafasProfile,
   Common,
@@ -26,173 +27,15 @@ import {
 } from 'types/HAFAS/StationBoard';
 import { TripSearchRequest, TripSearchResponse } from 'types/HAFAS/TripSearch';
 import axios from 'axios';
-import Crypto from 'crypto';
 import parseLocL from './helper/parseLocL';
 import parseProduct from './helper/parseProduct';
 
-function getSecret(hciChecksum: string) {
-  const enc = Buffer.from(hciChecksum, 'base64');
-  const key = Buffer.from([
-    97,
-    72,
-    54,
-    70,
-    56,
-    122,
-    82,
-    117,
-    105,
-    66,
-    110,
-    109,
-    51,
-    51,
-    102,
-    85,
-  ]);
-  const iv = Buffer.alloc(16);
-  const cipher = Crypto.createDecipheriv('aes-128-cbc', key, iv);
-  const secret = cipher.update(enc, undefined, 'ascii') + cipher.final('ascii');
-
-  return secret;
-}
-
-const mgateUrls = {
-  db: 'https://reiseauskunft.bahn.de/bin/mgate.exe',
-  oebb: 'https://fahrplan.oebb.at/bin/mgate.exe',
-  sncb: 'http://www.belgianrail.be/jp/sncb-nmbs-routeplanner/mgate.exe',
-  avv: 'https://auskunft.avv.de/bin/mgate.exe',
-  nahsh: 'https://nah.sh.hafas.de/bin/mgate.exe',
-  hvv: 'https://hvv-app.hafas.de/bin/mgate.exe',
-  bvg: 'https://bvg-apps.hafas.de/bin/mgate.exe',
-};
-const dbSecret = getSecret('rGhXPq+xAlvJd8T8cMnojdD0IoaOY53X7DPAbcXYe5g=');
-const hvvSecret = getSecret('ktlwfW4vVOf/LwJ4wsnENvzRQZf3WS9b1RMPbIQNEOw=');
-
-const extraAuth = {
-  db: (data: any) => {
-    const hasher = Crypto.createHash('md5');
-
-    hasher.update(JSON.stringify(data) + dbSecret);
-
-    return {
-      checksum: hasher.digest('hex'),
-    };
-  },
-  hvv: (data: any) => {
-    const micHasher = Crypto.createHash('md5');
-
-    micHasher.update(JSON.stringify(data));
-    const mic = micHasher.digest('hex');
-    const macHasher = Crypto.createHash('md5');
-
-    macHasher.update(mic + hvvSecret);
-
-    return { mic, mac: macHasher.digest('hex') };
-  },
-};
-
-const staticData = {
-  db: {
-    client: {
-      id: 'DB',
-      v: '19040000',
-      type: 'AND',
-      name: 'DB Navigator',
-    },
-    ext: 'DB.R19.04.a',
-    lang: 'de',
-    ver: '1.18',
-    auth: {
-      aid: 'n91dB8Z77MLdoR0K',
-      type: 'AID',
-    },
-  },
-  oebb: {
-    client: {
-      os: 'iOS 12.4',
-      id: 'OEBB',
-      v: '6020300',
-      type: 'IPH',
-      name: 'oebbADHOC',
-    },
-    lang: 'de',
-    ver: '1.18',
-    auth: { aid: 'OWDL4fE4ixNiPBBm', type: 'AID' },
-  },
-  sncb: {
-    client: {
-      os: 'iOS 12.4',
-      id: 'SNCB',
-      v: '4030200',
-      type: 'IPH',
-      name: 'sncb',
-    },
-    lang: 'de',
-    ver: '1.18',
-    auth: { aid: 'sncb-mobi', type: 'AID' },
-  },
-  avv: {
-    client: {
-      id: 'HAFAS',
-      type: 'WEB',
-      name: 'Test-Client',
-      v: '100',
-    },
-    lang: 'deu',
-    ver: '1.18',
-    auth: {
-      type: 'AID',
-      aid: '4vV1AcH3N511icH',
-    },
-  },
-  nahsh: {
-    client: {
-      os: 'iOS 12.4',
-      id: 'NAHSH',
-      v: '4000000',
-      type: 'IPH',
-      name: 'NAHSHAPPSTORE',
-    },
-    lang: 'de',
-    ver: '1.18',
-    auth: { aid: 'r0Ot9FLFNAFxijLW', type: 'AID' },
-  },
-  hvv: {
-    client: {
-      os: 'iOS 12.4',
-      id: 'HVV',
-      v: '4020100',
-      type: 'IPH',
-      name: 'HVVPROD_ADHOC',
-    },
-    lang: 'de',
-    ext: 'HVV.1',
-    ver: '1.18',
-    auth: {
-      aid: 'andcXUmC9Mq6hjrwDIGd2l3oiaMrTUzyH',
-      type: 'aid',
-    },
-  },
-  bvg: {
-    client: {
-      os: 'iOS 12.4',
-      id: 'BVG',
-      v: '6021600',
-      type: 'IPH',
-      name: 'Fahrinfo',
-    },
-    lang: 'de',
-    ver: '1.18',
-    auth: {
-      aid: 'Mz0YdF9Fgx0Mb9',
-      type: 'AID',
-    },
-  },
-};
-
-function createRequest(req: SingleHafasRequest, profile: AllowedHafasProfile) {
-  const data: any = staticData[profile];
+function createRequest(
+  req: SingleHafasRequest,
+  profileType: AllowedHafasProfile
+) {
+  const profile = HafasProfiles[profileType];
+  const data: any = profile.config;
 
   const auth = data.auth;
 
@@ -203,7 +46,7 @@ function createRequest(req: SingleHafasRequest, profile: AllowedHafasProfile) {
   data.auth = auth;
 
   // @ts-ignore 7053
-  const extraParam = extraAuth[profile] ? extraAuth[profile](data) : undefined;
+  const extraParam = profile.secret ? profile.secret(data) : undefined;
 
   return {
     data,
@@ -339,9 +182,13 @@ async function makeRequest<
   //   // eslint-disable-next-line no-console
   //   console.log(extraParam);
   // }
-  const r = (await axios.post<HafasResponse<HR>>(mgateUrls[profile], data, {
-    params: extraParam,
-  })).data;
+  const r = (await axios.post<HafasResponse<HR>>(
+    HafasProfiles[profile].url,
+    data,
+    {
+      params: extraParam,
+    }
+  )).data;
 
   if (r.err !== 'OK' || r.svcResL[0].err !== 'OK') {
     throw new HafasError(request, r, profile);
