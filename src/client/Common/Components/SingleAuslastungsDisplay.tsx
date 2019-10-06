f22e8119106d2aa150f898d97ae9f36202f05ce6
marudor <marudor@marudor.de>
2019-10-06 19:06:00 +0200

    Add Auslastungs to Legende

---
+++ src/client/Common/Components/SingleAuslastungsDisplay.tsx
@@ -0,0 +1,38 @@
+import { AuslastungsValue } from 'types/routing';
+import Close from '@material-ui/icons/Close';
+import Done from '@material-ui/icons/Done';
+import ErrorOutline from '@material-ui/icons/ErrorOutline';
+import Help from '@material-ui/icons/Help';
+import React from 'react';
+import useStyles from './SingleAuslastungsDisplay.style';
+import Warning from '@material-ui/icons/Warning';
+
+function getIcon(auslastung?: AuslastungsValue) {
+  switch (auslastung) {
+    case AuslastungsValue.Gering:
+      return <Done fontSize="inherit" />;
+    case AuslastungsValue.Hoch:
+      return <Warning fontSize="inherit" />;
+    case AuslastungsValue.SehrHoch:
+      return <ErrorOutline fontSize="inherit" />;
+    case AuslastungsValue.Ausgebucht:
+      return <Close fontSize="inherit" />;
+    default:
+      return <Help fontSize="inherit" />;
+  }
+}
+
+export type Props = {
+  auslastung?: AuslastungsValue;
+};
+const SingleAuslastungsDisplay = (props: Props) => {
+  const classes = useStyles(props);
+
+  return (
+    <span className={`${classes.icon} ${classes.color}`}>
+      {getIcon(props.auslastung)}
+    </span>
+  );
+};
+
+export default SingleAuslastungsDisplay;
