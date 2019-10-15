916f25d3f742bb4185950554e38fb40b9e43afff
marudor <marudor@marudor.de>
2019-10-15 19:08:01 +0200

    fix performance for list (#61)

--- src/client/Abfahrten/Components/Abfahrt/BaseAbfahrt.tsx
+++ src/client/Abfahrten/Components/Abfahrt/BaseAbfahrt.tsx
@@ -5,7 +5,7 @@ import cc from 'clsx';
 import End from './End';
 import Mid from './Mid';
 import Paper from '@material-ui/core/Paper';
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Reihung from 'Common/Components/Reihung';
 import SelectedDetailContainer from 'Abfahrten/container/SelectedDetailContainer';
 import Start from './Start';
@@ -44,56 +44,71 @@ const BaseAbfahrt = ({ abfahrt, wing, wingEnd, wingStart }: Props) => {
     shallowEqual
   );
 
-  return (
-    <Paper
-      square
-      id={abfahrt.id}
-      onClick={handleClick}
-      className={classes.main}
-    >
-      {wing && (
-        <span
-          className={cc(classes.wing, {
-            [classes.wingStart]: wingStart,
-            [classes.wingEnd]: wingEnd,
-          })}
-        />
-      )}
-      <div
-        data-testid={`abfahrt${abfahrt.train.trainCategory}${abfahrt.train.number}`}
-        className={classes.entry}
+  return useMemo(
+    () => (
+      <Paper
+        square
+        id={abfahrt.id}
+        onClick={handleClick}
+        className={classes.main}
       >
-        <div className={classes.entryMain}>
-          <Start
-            abfahrt={abfahrt}
-            detail={detail}
-            lineAndNumber={lineAndNumber}
-          />
-          <Mid abfahrt={abfahrt} detail={detail} />
-          <End abfahrt={abfahrt} detail={detail} />
-        </div>
-        {detail &&
-          abfahrt.departure &&
-          (abfahrt.reihung || abfahrt.hiddenReihung) && (
-            <Reihung
-              loadHidden={!abfahrt.reihung && abfahrt.hiddenReihung}
-              useZoom={useZoom}
-              showUIC={showUIC}
-              fahrzeugGruppe={fahrzeugGruppe}
-              trainNumber={abfahrt.train.number}
-              currentStation={abfahrt.currentStation.id}
-              scheduledDeparture={abfahrt.departure.scheduledTime}
-            />
-          )}
-        {detail && (
-          <div
-            data-testid="scrollMarker"
-            id={`${abfahrt.id}Scroll`}
-            className={classes.scrollMarker}
+        {wing && (
+          <span
+            className={cc(classes.wing, {
+              [classes.wingStart]: wingStart,
+              [classes.wingEnd]: wingEnd,
+            })}
           />
         )}
-      </div>
-    </Paper>
+        <div
+          data-testid={`abfahrt${abfahrt.train.trainCategory}${abfahrt.train.number}`}
+          className={classes.entry}
+        >
+          <div className={classes.entryMain}>
+            <Start
+              abfahrt={abfahrt}
+              detail={detail}
+              lineAndNumber={lineAndNumber}
+            />
+            <Mid abfahrt={abfahrt} detail={detail} />
+            <End abfahrt={abfahrt} detail={detail} />
+          </div>
+          {detail &&
+            abfahrt.departure &&
+            (abfahrt.reihung || abfahrt.hiddenReihung) && (
+              <Reihung
+                loadHidden={!abfahrt.reihung && abfahrt.hiddenReihung}
+                useZoom={useZoom}
+                showUIC={showUIC}
+                fahrzeugGruppe={fahrzeugGruppe}
+                trainNumber={abfahrt.train.number}
+                currentStation={abfahrt.currentStation.id}
+                scheduledDeparture={abfahrt.departure.scheduledTime}
+              />
+            )}
+          {detail && (
+            <div
+              data-testid="scrollMarker"
+              id={`${abfahrt.id}Scroll`}
+              className={classes.scrollMarker}
+            />
+          )}
+        </div>
+      </Paper>
+    ),
+    [
+      abfahrt,
+      classes,
+      detail,
+      fahrzeugGruppe,
+      handleClick,
+      lineAndNumber,
+      showUIC,
+      useZoom,
+      wing,
+      wingEnd,
+      wingStart,
+    ]
   );
 };
 
---
+++ src/client/Abfahrten/hooks/useAllTrainTypes.ts
@@ -0,0 +1,27 @@
+import { useAbfahrtenSelector } from 'useSelector';
+import { useMemo } from 'react';
+
+const defaultTypes = ['ICE', 'IC', 'EC', 'RE', 'RB', 'S'];
+
+export default () => {
+  const departures = useAbfahrtenSelector(state => state.abfahrten.departures);
+
+  return useMemo(() => {
+    const typeSet = new Set<string>(defaultTypes);
+
+    if (departures) {
+      departures.lookahead.forEach(a => {
+        if (a.train.type) {
+          typeSet.add(a.train.type);
+        }
+      });
+      departures.lookbehind.forEach(a => {
+        if (a.train.type) {
+          typeSet.add(a.train.type);
+        }
+      });
+    }
+
+    return Array.from(typeSet);
+  }, [departures]);
+};
