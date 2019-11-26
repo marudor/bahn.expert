0a9b13e1735e3ae2ae9592b54fea54c348472508
marudor <marudor@marudor.de>
2019-11-26 18:08:46 +0100

    fix tz issues

--- src/server/Reihung.ts
+++ src/server/Reihung.ts
@@ -9,11 +9,11 @@ import {
   Wagenreihung,
   WagenreihungStation,
 } from 'types/reihung';
-import { format } from 'date-fns-tz';
+import { format, isAfter, subDays } from 'date-fns';
 import { getAbfahrten } from './Abfahrten';
 import { getAP } from 'server/Wifi';
 import { groupBy, maxBy, minBy } from 'lodash';
-import { isAfter, subDays } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import axios from 'axios';
 
 const getATBR = (
@@ -400,9 +400,10 @@ function enrichFahrzeug(fahrzeug: Fahrzeug, gruppe: Fahrzeuggruppe) {
 
 // https://www.apps-bahn.de/wr/wagenreihung/1.0/6/201802021930
 export async function wagenreihung(trainNumber: string, date: number) {
-  const parsedDate = format(date, 'yyyyMMddHHmm', {
-    timeZone: 'Europe/Berlin',
-  });
+  const parsedDate = format(
+    utcToZonedTime(date, 'Europe/Berlin'),
+    'yyyyMMddHHmm'
+  );
 
   let info: Wagenreihung;
 
