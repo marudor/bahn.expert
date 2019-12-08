dc41c63fafd0dc5dc303cd2cf613af1dc5c8609f
marudor <marudor@marudor.de>
2019-12-08 11:35:30 +0100

    fix jest tests

--- src/client/ThemeWrap.tsx
+++ src/client/ThemeWrap.tsx
@@ -4,7 +4,7 @@ import {
   ThemeProvider,
 } from '@material-ui/styles';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import { SheetsRegistry } from 'jss';
+import { Rule, SheetsRegistry, StyleSheet } from 'jss';
 import App from './App';
 import DateFnsUtils from '@date-io/date-fns';
 import deLocale from 'date-fns/locale/de';
@@ -20,7 +20,16 @@ interface Props {
 const ThemeWrap = ({ children = <App />, sheetsRegistry }: Props) => {
   const { theme } = ThemeContainer.useContainer();
 
-  const generateClassName = useMemo(() => createGenerateClassName(), []);
+  let generateClassName = useMemo(() => createGenerateClassName(), []);
+
+  if (global.TEST) {
+    generateClassName = (rule: Rule, sheet?: StyleSheet<string>) => {
+      // @ts-ignore
+      const name = `${sheet.options.name}-${rule.key}`;
+
+      return name;
+    };
+  }
 
   return (
     <StylesProvider
