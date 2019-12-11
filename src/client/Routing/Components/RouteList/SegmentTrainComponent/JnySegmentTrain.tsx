59ff3e7035f7c38b022e806928b44968f7670583
marudor <marudor@marudor.de>
2019-12-11 09:18:45 +0100

    remove duplicate details link on routing (#152)
    Former-commit-id: c02c85962ffad7953678a8a6604a80eafa5b8d6e

--- src/client/Routing/Components/RouteList/SegmentTrainComponent/JnySegmentTrain.tsx
+++ src/client/Routing/Components/RouteList/SegmentTrainComponent/JnySegmentTrain.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { Route$JourneySegmentTrain } from 'types/routing';
 import AuslastungsDisplay from 'Common/Components/AuslastungsDisplay';
 import cc from 'clsx';
@@ -6,7 +5,6 @@ import DetailsLink from 'Common/Components/Details/DetailsLink';
 import React, { MouseEvent } from 'react';
 import Reihung from 'Common/Components/Reihung';
 import StopList from 'Routing/Components/RouteList/StopList';
-import stopPropagation from 'Common/stopPropagation';
 import useStyles from './style';
 
 interface Props {
@@ -32,12 +30,6 @@ const JnySegmentTrain = ({
             train={segment.train}
             initialDeparture={segment.departure.scheduledTime}
           />
-          <Link
-            onClick={stopPropagation}
-            to={`/details/${segment.train.type} ${segment.train.number}/${segment.departure.scheduledTime}`}
-          >
-            Details
-          </Link>
         </span>
         <span className={cc(classes.trainMargin, classes.destination)}>
           {segment.finalDestination}
