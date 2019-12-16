bbe453990702e8b1443d7256072403b5295512f7
marudor <marudor@marudor.de>
2019-12-16 08:52:56 +0100

    CheckInLink knows state

--- src/client/Common/Components/CheckInLink/index.tsx
+++ src/client/Common/Components/CheckInLink/index.tsx
@@ -1,20 +1,21 @@
 import { Abfahrt } from 'types/iris';
 import { CheckInType } from 'Common/config';
+import CommonConfigContainer from 'Common/container/CommonConfigContainer';
 import React from 'react';
 import TravelynxLink from './TravelynxLink';
 
 interface Props {
   abfahrt: Abfahrt;
-  type: CheckInType;
 }
 
-const CheckInLink = ({ abfahrt, type }: Props) => {
-  switch (type) {
-    case CheckInType.Travelynx:
-      return <TravelynxLink abfahrt={abfahrt} />;
-    default:
-      return null;
+const CheckInLink = ({ abfahrt }: Props) => {
+  const checkInType = CommonConfigContainer.useContainer().config.checkIn;
+
+  if (checkInType === CheckInType.Travelynx) {
+    return <TravelynxLink abfahrt={abfahrt} />;
   }
+
+  return null;
 };
 
 export default CheckInLink;
--- test/client/Common/Components/CheckInLink.test.ts
+++ test/client/Common/Components/CheckInLink.test.ts
@@ -21,58 +21,93 @@ describe('CheckInLink', () => {
     it('Renders Travellynx if arrival in 20 Minutes', () => {
       clock.setSystemTime(parse('22.07.2019 14:19', 'dd.MM.yyyy HH:mm', 0));
 
-      const { getByTestId } = render(CheckInLink, {
-        type: CheckInType.Travelynx,
-        abfahrt: MockAbfahrt,
-      });
+      const { getByTestId } = render(
+        CheckInLink,
+        {
+          abfahrt: MockAbfahrt,
+        },
+        {
+          commonConfig: {
+            checkIn: CheckInType.Travelynx,
+          },
+        }
+      );
 
       getByTestId('travellynxlink');
     });
     it('Renders Travellynx if departure in 20 Minutes and arrival not existant', () => {
       clock.setSystemTime(parse('22.07.2019 14:19', 'dd.MM.yyyy HH:mm', 0));
 
-      const { getByTestId } = render(CheckInLink, {
-        type: CheckInType.Travelynx,
-        abfahrt: {
-          ...MockAbfahrt,
-          arrival: undefined,
+      const { getByTestId } = render(
+        CheckInLink,
+        {
+          abfahrt: {
+            ...MockAbfahrt,
+            arrival: undefined,
+          },
         },
-      });
+        {
+          commonConfig: {
+            checkIn: CheckInType.Travelynx,
+          },
+        }
+      );
 
       getByTestId('travellynxlink');
     });
     it('Renders no link if arrival >30 Minutes away', () => {
       clock.setSystemTime(parse('22.07.2019 14:08', 'dd.MM.yyyy HH:mm', 0));
 
-      const { queryByTestId } = render(CheckInLink, {
-        type: CheckInType.Travelynx,
-        abfahrt: MockAbfahrt,
-      });
+      const { queryByTestId } = render(
+        CheckInLink,
+        {
+          abfahrt: MockAbfahrt,
+        },
+        {
+          commonConfig: {
+            checkIn: CheckInType.Travelynx,
+          },
+        }
+      );
 
       expect(queryByTestId('travellynxlink')).toBeNull();
     });
     it('Renders link if arrival >30 Minutes but planned arrival <30', () => {
       clock.setSystemTime(parse('22.07.2019 14:19', 'dd.MM.yyyy HH:mm', 0));
-      const { getByTestId } = render(CheckInLink, {
-        type: CheckInType.Travelynx,
-        abfahrt: {
-          ...MockAbfahrt,
-          arrival: {
-            ...MockAbfahrt.arrival,
-            time: parse('22.07.2019 17:52', 'dd.MM.yyyy HH:mm', 0).getTime(),
+      const { getByTestId } = render(
+        CheckInLink,
+        {
+          abfahrt: {
+            ...MockAbfahrt,
+            arrival: {
+              ...MockAbfahrt.arrival,
+              time: parse('22.07.2019 17:52', 'dd.MM.yyyy HH:mm', 0).getTime(),
+            },
           },
         },
-      });
+        {
+          commonConfig: {
+            checkIn: CheckInType.Travelynx,
+          },
+        }
+      );
 
       getByTestId('travellynxlink');
     });
   });
 
   it('Renders none', () => {
-    const { queryByTestId } = render(CheckInLink, {
-      type: CheckInType.None,
-      abfahrt: MockAbfahrt,
-    });
+    const { queryByTestId } = render(
+      CheckInLink,
+      {
+        abfahrt: MockAbfahrt,
+      },
+      {
+        commonConfig: {
+          checkIn: CheckInType.None,
+        },
+      }
+    );
 
     expect(queryByTestId('travellynxlink')).toBeNull();
   });
