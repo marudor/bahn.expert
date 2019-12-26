ccffed2f654fc396ad595e5542613e7796f7fba4
Milan Pässler <me@pbb.lc>
2019-12-26 23:13:29 +0100

    refactor trainmap

--- src/client/Map/TrainMap.tsx
+++ src/client/Map/TrainMap.tsx
@@ -1,22 +1,58 @@
-import 'leaflet/dist/leaflet.css';
-import { Map, Marker, TileLayer, Tooltip } from 'react-leaflet';
+import './style.css';
+import 'ol/ol.css';
+
+import { OlMap } from 'openlayers-react';
 import { ParsedJourneyGeoPosResponse } from 'types/HAFAS/JourneyGeoPos';
 import axios from 'axios';
-// @ts-ignore
-import icon from 'leaflet/dist/images/marker-icon.png';
-// @ts-ignore
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import L from 'leaflet';
 import React, { useEffect, useState } from 'react';
 
-const DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-  iconSize: [24, 36],
-  iconAnchor: [12, 36],
+import {
+  Circle as CircleStyle,
+  Fill as FillStyle,
+  Stroke as StrokeStyle,
+  Style,
+  Text as TextStyle,
+} from 'ol/style';
+import { defaults as defaultControls } from 'ol/control';
+import { Feature, View as OlView } from 'ol';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { transform } from 'ol/proj';
+import { Vector as VectorSource, XYZ as XYZSource } from 'ol/source';
+import Point from 'ol/geom/Point';
+
+const vectorSource = new VectorSource();
+
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  zIndex: 3,
 });
 
-L.Marker.prototype.options.icon = DefaultIcon;
+const mapOptions = {
+  controls: defaultControls(),
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url:
+          'https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.webp',
+        attributions:
+          '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a> | <a href="https://carto.com/attribution">© CARTO</a>',
+      }),
+    }),
+    new TileLayer({
+      source: new XYZSource({
+        url: 'https://a.tiles.openrailwaymap.org/maxspeed/{z}/{x}/{y}.webp',
+        attributions:
+          ' | <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a>',
+      }),
+      zIndex: 2,
+    }),
+    vectorLayer,
+  ],
+  view: new OlView({
+    center: transform([9.955472, 50.954032], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 7,
+  }),
+};
 
 async function fetchPositions() {
   const result: ParsedJourneyGeoPosResponse = (
@@ -66,41 +102,65 @@ const Positions = () => {
   }, []);
 
   if (positions) {
-    return (
-      <>
-        {positions.map(p => (
-          <Marker key={p.jid} position={[p.position.lat, p.position.lng]}>
-            <Tooltip permanent>{p.train.name}</Tooltip>
-          </Marker>
-        ))}
-      </>
+    vectorSource.clear();
+    let i = 0;
+
+    vectorSource.addFeatures(
+      positions.map(p => {
+        p.train.name = p.train.name.replace(/\s*([0-9]+)/, '\n$1');
+        const feat = new Feature({
+          geometry: new Point(
+            transform(
+              [p.position.lng, p.position.lat],
+              'EPSG:4326',
+              'EPSG:3857'
+            )
+          ),
+        });
+
+        feat.setStyle(
+          new Style({
+            image: new CircleStyle({
+              radius:
+                15 +
+                p.train.name
+                  .split('\n')
+                  .slice(1)
+                  .join(' ').length *
+                  3,
+              fill: new FillStyle({ color: 'white' }),
+              stroke: new StrokeStyle({
+                color: 'black',
+                width: 1,
+              }),
+            }),
+            text: new TextStyle({
+              font: 'bold 14px sans-serif',
+              text: p.train.name,
+              fill: new FillStyle({ color: '#dd0000' }),
+            }),
+            zIndex: 5 + i,
+          })
+        );
+        i += 1;
+
+        feat.setId(p.jid);
+
+        return feat;
+      })
     );
   }
 
   return null;
 };
 
-const attribution =
-  '<a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>, Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a> <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a> and OpenStreetMap';
-
 const TrainMap = () => {
   return (
-    <>
-      <Map
-        style={{ flex: 1 }}
-        viewport={{
-          center: [50.954032, 9.955472],
-          zoom: 7,
-        }}
-      >
-        <TileLayer
-          attribution={attribution}
-          url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png"
-        />
-        <TileLayer url="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png" />
+    <div>
+      <OlMap initialMapOptions={mapOptions} mapId={'MyMap'}>
         <Positions />
-      </Map>
-    </>
+      </OlMap>
+    </div>
   );
 };
 
