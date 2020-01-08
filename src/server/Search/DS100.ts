68243a0fd41b90ecec1db6f66f5078716e1cc9b1
marudor <marudor@marudor.de>
2020-01-08 22:41:35 +0100

    fix ds100 search

--- src/server/Search/DS100.ts
+++ src/server/Search/DS100.ts
@@ -2,13 +2,17 @@ import { getSingleStation } from 'server/Abfahrten/station';
 import { Station } from 'types/station';
 
 export default async function(ds100: string): Promise<Station | undefined> {
-  const timetablesStation = await getSingleStation(ds100);
+  try {
+    const timetablesStation = await getSingleStation(ds100);
 
-  if (timetablesStation.ds100.toLowerCase() === ds100.toLowerCase()) {
-    return {
-      DS100: timetablesStation.ds100,
-      title: timetablesStation.name,
-      id: timetablesStation.eva,
-    };
+    if (timetablesStation.ds100.toLowerCase() === ds100.toLowerCase()) {
+      return {
+        DS100: timetablesStation.ds100,
+        title: timetablesStation.name,
+        id: timetablesStation.eva,
+      };
+    }
+  } catch {
+    // No result if it throws
   }
 }
