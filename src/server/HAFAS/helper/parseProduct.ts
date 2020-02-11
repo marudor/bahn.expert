f6a4513f2662014aa03d97d55a85cb4ed489fe85
marudor <marudor@marudor.de>
2020-02-11 11:30:19 +0100

    fix: if we know admin value of a product ensure it starts with 80 (DE) (#244)
    Former-commit-id: bb3eee4de620d851f974e4289974a49c58a173f3

--- src/server/HAFAS/helper/parseProduct.ts
+++ src/server/HAFAS/helper/parseProduct.ts
@@ -6,6 +6,7 @@ export default (product: ProdL, common: Common): ParsedProduct => ({
     product.nameS ||
     product.matchId ||
     (product.prodCtx && (product.prodCtx.line || product.prodCtx.lineId)),
+  admin: product.prodCtx?.admin,
   number: product.prodCtx?.num ?? product.number,
   type: product.prodCtx && (product.prodCtx.catOut || product.prodCtx.catOutL),
   operator: product.oprX !== undefined ? common.opL[product.oprX] : undefined,
