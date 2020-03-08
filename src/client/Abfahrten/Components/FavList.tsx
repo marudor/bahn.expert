9c1b0c2b6a2a44c93cb49ff397a85b8bc3c04703
ndo@ndo1 <yo@ni.co.de>
2020-03-09 00:49:50 +0100

    fix: last failing test

--- src/client/Abfahrten/Components/FavList.tsx
+++ src/client/Abfahrten/Components/FavList.tsx
@@ -47,7 +47,9 @@ const FavList = ({ staticContext }: Props) => {
 
     return values
       .sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1))
-      .map(fav => <FavEntry key={fav.id} fav={fav} />);
+      .map(fav => (
+        <FavEntry data-testid="realFavEntry" key={fav.id} fav={fav} />
+      ));
   }, [favs]);
   const { error } = AbfahrtenContainer.useContainer();
   const [savedError] = useState(error);
