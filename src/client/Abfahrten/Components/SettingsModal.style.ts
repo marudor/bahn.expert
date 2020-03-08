28f1cb559af7ea863c14a83edfd91495226e2a9b
ndo@ndo1 <yo@ni.co.de>
2020-03-08 23:12:50 +0100

    style: settings modal

--- src/client/Abfahrten/Components/SettingsModal.style.ts
+++ src/client/Abfahrten/Components/SettingsModal.style.ts
@@ -4,11 +4,26 @@ export default makeStyles({
   main: {
     display: 'flex',
     flexDirection: 'column',
+    width: '80%',
+    maxWidth: '450px',
+    margin: '0 auto',
   },
   autoUpdate: {
     width: '3em',
   },
   label: {
     justifyContent: 'space-between',
+    flexDirection: 'row-reverse',
+    marginRight: '0',
+    marginLeft: '0',
+    marginBottom: '15px',
+    width: '100%',
+  },
+  header: {
+    textAlign: 'center',
+    '& h2': {
+      fontSize: '1.5rem',
+      fontWeight: '600',
+    },
   },
 });
