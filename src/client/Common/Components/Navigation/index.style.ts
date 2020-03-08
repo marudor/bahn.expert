155483dda9d954597310bc89e99d72ab12ae4d08
ndo@ndo1 <yo@ni.co.de>
2020-03-08 17:42:46 +0100

    style: menu spacing

--- src/client/Common/Components/Navigation/index.style.ts
+++ src/client/Common/Components/Navigation/index.style.ts
@@ -2,8 +2,15 @@ import { makeStyles } from '@material-ui/styles';
 
 export default makeStyles(() => ({
   drawer: {
+    width: '230px',
     '& a': {
       color: 'inherit',
     },
+    '& .MuiListItem-button': {
+      padding: '20px 20px',
+    },
+  },
+  header: {
+    textAlign: 'center',
   },
 }));
