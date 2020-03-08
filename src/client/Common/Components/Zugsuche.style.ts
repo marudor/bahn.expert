dcd9059bb92c4376a79c0788820facbe7d20bc47
ndo@ndo1 <yo@ni.co.de>
2020-03-09 00:14:47 +0100

    style: zugsuche

--- src/client/Common/Components/Zugsuche.style.ts
+++ src/client/Common/Components/Zugsuche.style.ts
@@ -4,4 +4,18 @@ export default makeStyles({
   main: {
     minWidth: '40%',
   },
+  searchButton: {
+    border: '2px solid #585858',
+    borderRadius: '5px',
+    height: '45px',
+    backgroundColor: 'transparent',
+    margin: '10px',
+    width: '95%',
+  },
+  header: {
+    textAlign: 'center',
+  },
+  searchInput: {
+    margin: '20px',
+  },
 });
