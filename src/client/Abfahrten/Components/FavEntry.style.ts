54570bd4e1ed469d89968cd97eeb5e0370d1ebaa
ndo@ndo1 <yo@ni.co.de>
2020-03-09 09:56:07 +0100

    style: favEntry responsive sizing

--- src/client/Abfahrten/Components/FavEntry.style.ts
+++ src/client/Abfahrten/Components/FavEntry.style.ts
@@ -6,15 +6,15 @@ export default makeStyles(theme => ({
     marginBottom: 1,
     flexShrink: 0,
     paddingLeft: '.5em',
-    fontSize: '2em',
+    fontSize: '1.6em',
     paddingRight: '.5em',
     color: theme.palette.text.primary,
     display: 'flex',
     justifyContent: 'space-between',
     height: '60px',
     alignItems: 'center',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '1.6rem',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '2rem',
     },
     '& > a': {
       colors: theme.palette.text.primary,
