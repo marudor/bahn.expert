3b37007641d2a5a9d427648b55b35bf91b247137
ndo@ndo1 <yo@ndo.dev>
2020-03-10 10:45:56 +0100

    style: zugsuche native material buttons + icons on input

--- src/client/Common/Components/Zugsuche.style.ts
+++ src/client/Common/Components/Zugsuche.style.ts
@@ -5,17 +5,30 @@ export default makeStyles({
     minWidth: '40%',
   },
   searchButton: {
-    border: '2px solid #585858',
-    borderRadius: '5px',
     height: '45px',
-    backgroundColor: 'transparent',
     margin: '10px',
     width: '95%',
   },
   header: {
     textAlign: 'center',
+    padding: '16px 24px 0px 24px',
   },
   searchInput: {
     margin: '20px',
+  },
+  dateInputWrapper: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  zugInputWrapper: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  inputIcon: {
+    position: 'absolute',
+    right: '20px',
+    top: '30px',
   },
 });
