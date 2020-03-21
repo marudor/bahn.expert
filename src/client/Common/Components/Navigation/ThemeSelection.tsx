97378936356861ecef289e4e815d4e20b7779903
Nico Domino <yo@ni.co.de>
2020-03-21 12:06:41 +0100

    style: Menu (#275)
    * style: menu spacing
    
    * style: active theme display
    
    * style: menu icons
    
    * fix: lint errors
    
    Co-authored-by: marudor <marudor@marudor.de>
    Former-commit-id: 707d2e5eb45106d13565e768e09f015635b0ad89
    Former-commit-id: 2d02b446904b8b9d535d66a58c6105cb1a406894

--- src/client/Common/Components/Navigation/ThemeSelection.tsx
+++ src/client/Common/Components/Navigation/ThemeSelection.tsx
@@ -1,8 +1,17 @@
+import {
+  Badge,
+  Collapse,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@material-ui/core';
 import { capitalize } from 'lodash';
-import { Collapse, List, ListItem, ListItemText } from '@material-ui/core';
 import { ThemeType } from 'client/Themes/type';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import PaletteIcon from '@material-ui/icons/Palette';
 import React, { SyntheticEvent, useCallback, useState } from 'react';
 import ThemeContainer from 'Common/container/ThemeContainer';
 import useStyles from './ThemeSelection.style';
@@ -34,17 +43,16 @@ const ThemeSelection = () => {
   return (
     <>
       <ListItem data-testid="themes" button onClick={toggle}>
+        <ListItemIcon>
+          <PaletteIcon />
+        </ListItemIcon>
         <ListItemText primary="Themes" />
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItem>
       <Collapse in={open} timeout="auto" unmountOnExit>
         <List disablePadding className={classes.nested} data-testid="themeList">
           {Object.values(ThemeType).map(themeOption => {
-            let name = capitalize(themeOption);
-
-            if (themeOption === themeType) {
-              name = `* ${name}`;
-            }
+            const name = capitalize(themeOption);
 
             return (
               <ListItem
@@ -53,7 +61,13 @@ const ThemeSelection = () => {
                 onClick={selectTheme}
                 data-value={themeOption}
               >
-                <ListItemText primary={name} />
+                {themeOption === themeType ? (
+                  <Badge color="primary" variant="dot">
+                    <Typography>{name}</Typography>
+                  </Badge>
+                ) : (
+                  <ListItemText primary={name} />
+                )}
               </ListItem>
             );
           })}
--- src/client/Common/Components/Navigation/index.tsx
+++ src/client/Common/Components/Navigation/index.tsx
@@ -2,11 +2,16 @@ import { Link } from 'react-router-dom';
 import {
   List,
   ListItem,
+  ListItemIcon,
   ListItemText,
   SwipeableDrawer,
 } from '@material-ui/core';
+import AlarmOnOutlinedIcon from '@material-ui/icons/AlarmOnOutlined';
+import ExploreIcon from '@material-ui/icons/Explore';
+import InfoIcon from '@material-ui/icons/Info';
 import NavigationContext from './NavigationContext';
 import React, { ReactNode, useCallback, useMemo, useState } from 'react';
+import SearchIcon from '@material-ui/icons/Search';
 import ThemeSelection from './ThemeSelection';
 import useStyles from './index.style';
 import Zugsuche from 'Common/Components/Zugsuche';
@@ -31,20 +36,30 @@ const Navigation = ({ children }: Props) => {
   return (
     <NavigationContext.Provider value={navigationContext}>
       <SwipeableDrawer open={open} onClose={toggleDrawer} onOpen={toggleDrawer}>
+        <h3 className={classes.header}>BahnhofsAbfahrten</h3>
         <List className={classes.drawer} onClick={toggleDrawer}>
           <Link to="/">
             <ListItem button>
+              <ListItemIcon>
+                <AlarmOnOutlinedIcon />
+              </ListItemIcon>
               <ListItemText primary="Abfahrten" />
             </ListItem>
           </Link>
           <Link to="/routing">
             <ListItem button>
+              <ListItemIcon>
+                <ExploreIcon />
+              </ListItemIcon>
               <ListItemText primary="Routing" />
             </ListItem>
           </Link>
           <Zugsuche>
             {toggle => (
               <ListItem button onClick={toggle}>
+                <ListItemIcon>
+                  <SearchIcon />
+                </ListItemIcon>
                 <ListItemText primary="Zugsuche" />
               </ListItem>
             )}
@@ -52,6 +67,9 @@ const Navigation = ({ children }: Props) => {
           <ThemeSelection />
           <Link to="/about">
             <ListItem button>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
               <ListItemText primary="About" />
             </ListItem>
           </Link>
