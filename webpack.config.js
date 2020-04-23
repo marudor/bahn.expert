8af69d88651b941fca4d3718612423ba2c2f86f8
marudor <marudor@marudor.de>
2020-04-23 17:54:30 +0200

    chore: cleaning up some dependencies, removing packtrackr

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,12 @@ const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const LoadablePlugin = require('@loadable/webpack-plugin');
-const PacktrackerPlugin = require('@packtracker/webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const SentryCliPlugin = require('@sentry/webpack-plugin');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 const plugins = [
-  new BundleAnalyzerPlugin({
-    analyzerMode: 'static',
-    openAnalyzer: false,
-  }),
-  new PacktrackerPlugin({
-    fail_build: true,
-    upload: process.env.sendStats === 'true',
-  }),
   new LoadablePlugin(),
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
