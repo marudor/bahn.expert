e92af5f532af846dfefb5ec7fa4cbd13eff2497d
marudor <marudor@marudor.de>
2020-05-27 07:46:50 +0200

    feat: umi-request for sbb

--- packages/sbb/routing.ts
+++ packages/sbb/routing.ts
@@ -1,5 +1,5 @@
-import { axios } from 'sbb';
 import { format } from 'date-fns';
+import { request } from 'sbb';
 import { RoutingOptions } from 'sbb/types/routing';
 
 export async function routing({
@@ -7,20 +7,18 @@ export async function routing({
   destination,
   time = Date.now(),
 }: RoutingOptions) {
-  const result = (
-    await axios.get(
-      `/unauth/fahrplanservice/v1/verbindungen/s/${start}/s/${destination}/ab/${format(
-        time,
-        'yyyy-MM-dd'
-      )}/${format(time, 'HH-mm')}`,
-      {
-        params: {
-          vonId: start,
-          nachId: destination,
-        },
-      }
-    )
-  ).data;
+  const result = await request.get(
+    `/unauth/fahrplanservice/v1/verbindungen/s/${start}/s/${destination}/ab/${format(
+      time,
+      'yyyy-MM-dd'
+    )}/${format(time, 'HH-mm')}`,
+    {
+      params: {
+        vonId: start,
+        nachId: destination,
+      },
+    }
+  );
 
   return result;
 }
