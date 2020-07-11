5afaf64773b442a43ee60c5f4e03d4c1f19ea738
marudor <marudor@marudor.de>
2020-07-11 12:09:44 +0200

    fix: e2e tests

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,9 +2,9 @@ global.M = require('react').createElement;
 global.MF = require('react').Fragment;
 
 global.PROD = process.env.NODE_ENV === 'production';
-global.BASE_URL = `${global.PROD ? 'https://' : 'http://'}${
-  process.env.BASE_URL || 'localhost:9042'
-}`;
+global.BASE_URL = `${
+  global.PROD && !process.env.TEST_RUN ? 'https://' : 'http://'
+}${process.env.BASE_URL || 'localhost:9042'}`;
 global.SERVER = true;
 global.IMPRINT = require('./imprint');
 global.VERSION = require('./version').default;
