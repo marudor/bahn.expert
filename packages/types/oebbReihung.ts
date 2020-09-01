75271c50aca201b39a91daa8e0745dc3bac4e409
marudor <marudor@marudor.de>
2020-09-01 12:06:07 +0200

    feat: oebb Reihung types. Implentation follows once API is reliable

---
+++ packages/types/oebbReihung.ts
@@ -0,0 +1,73 @@
+/**
+ *
+ * url: https://live.oebb.at/backend/api/train/<TrainType> <TrainNumber>/stationEva/<EVAId>/departure/<Departure Date as dd.MM.yyyy>
+ * example: https://live.oebb.at/backend/api/train/ICE%2028/stationEva/8103000/departure/01.09.2020
+ */
+
+export interface OebbWagon {
+  ordnungsNummer: number;
+  uicNummer: string;
+  laengeUeberPuffer: number;
+  triebfahrzeug: boolean;
+  speisewagen: boolean;
+  businessClass: number;
+  firstClass: number;
+  secondClass: number;
+  schlafplaetze: number;
+  liegeplaetze: number;
+  autoreisezug: boolean;
+  kinderspielwagen: boolean;
+  kinderkino: boolean;
+  rollstuhlgerecht: boolean;
+  fahrradmitnahme: boolean;
+  abgesperrt: boolean;
+  origin: OebbStation;
+  destination: OebbStation;
+  ruhebereich: boolean;
+  infoPoint: boolean;
+}
+
+export interface OebbStation {
+  name: string;
+  evaCode: string;
+}
+
+export interface OebbEgress {
+  Bahnsteig: string;
+  Distanz: number;
+  Type: string;
+  DB640Code: string;
+  name: string;
+}
+
+export interface OebbSector {
+  sectorName: string;
+  length: number;
+}
+
+export interface OebbHaltepunkt {
+  haltepunktInMeters: unknown | null;
+  departureDirectionSectorA: unknown | null;
+}
+
+export interface OebbPlatform {
+  platform: string;
+  haltepunkt: OebbHaltepunkt;
+  sectors: OebbSector[];
+  egresses: OebbEgress[];
+}
+
+export interface OebbReihung {
+  trainName: string;
+  /**
+   * always true so far?
+   */
+  hasWifi: boolean;
+  scheduledDeparture: string;
+  actualDeparture: string | null;
+  scheduledArrival: string;
+  actualArrival: string | null;
+  tranStation: OebbStation;
+  platform: OebbPlatform;
+  wagons: unknown[];
+}
