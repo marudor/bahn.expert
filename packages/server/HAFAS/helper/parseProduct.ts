0ca8eaec5aa86fb4a9833651df9af043eb766085
marudor <marudor@marudor.de>
2020-10-13 15:32:50 +0200

    chore: get list of possible operators (#425)

--- packages/server/HAFAS/helper/parseProduct.ts
+++ packages/server/HAFAS/helper/parseProduct.ts
@@ -1,17 +1,37 @@
+import { CacheDatabases, createNewCache } from 'server/cache';
 import type { Common, ParsedProduct, ProdL } from 'types/HAFAS';
 
-export default (product: ProdL, common: Common): ParsedProduct => ({
-  name: product.addName || product.name,
-  line:
-    product.prodCtx?.line ||
-    product.prodCtx?.lineId ||
-    product.prodCtx?.matchId ||
-    product.matchId ||
-    product.nameS,
-  admin: product.prodCtx?.admin,
-  number: product.prodCtx?.num ?? product.number,
-  type: product.prodCtx && (product.prodCtx.catOut || product.prodCtx.catOutL),
-  operator: product.oprX !== undefined ? common.opL[product.oprX] : undefined,
-  // @ts-expect-error just debug
-  raw: global.PROD ? undefined : product,
-});
+const operatorCache = createNewCache<string, boolean>(
+  100 * 6 * 60 * 60,
+  CacheDatabases.Operators,
+);
+
+export default (product: ProdL, common: Common): ParsedProduct => {
+  const operator =
+    product.oprX !== undefined ? common.opL[product.oprX] : undefined;
+
+  if (operator?.name) {
+    void operatorCache.get(operator.name).then((val) => {
+      if (!val) {
+        void operatorCache.set(operator.name, true);
+      }
+    });
+  }
+
+  return {
+    name: product.addName || product.name,
+    line:
+      product.prodCtx?.line ||
+      product.prodCtx?.lineId ||
+      product.prodCtx?.matchId ||
+      product.matchId ||
+      product.nameS,
+    admin: product.prodCtx?.admin,
+    number: product.prodCtx?.num ?? product.number,
+    type:
+      product.prodCtx && (product.prodCtx.catOut || product.prodCtx.catOutL),
+    operator,
+    // @ts-expect-error just debug
+    raw: global.PROD ? undefined : product,
+  };
+};
