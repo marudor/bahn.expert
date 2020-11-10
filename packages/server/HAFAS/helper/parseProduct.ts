1d75010cfdbb740b0a969b7fdb76214690a7c117
marudor <marudor@marudor.de>
2020-11-10 14:44:41 +0100

    chore: remove debug code (#435)

--- packages/server/HAFAS/helper/parseProduct.ts
+++ packages/server/HAFAS/helper/parseProduct.ts
@@ -1,36 +1,8 @@
-import { CacheDatabases, createNewCache } from 'server/cache';
 import type { Common, ParsedProduct, ProdL } from 'types/HAFAS';
-
-const operatorCache = createNewCache<string, boolean>(
-  100 * 6 * 60 * 60,
-  CacheDatabases.Operators,
-);
-
-const categoryCache = createNewCache<string, boolean>(
-  100 * 6 * 60 * 60,
-  CacheDatabases.Categories,
-);
 
 export default (product: ProdL, common: Common): ParsedProduct => {
   const operator =
     product.oprX !== undefined ? common.opL[product.oprX] : undefined;
-
-  if (operator?.name) {
-    void operatorCache.get(operator.name).then((val) => {
-      if (!val) {
-        void operatorCache.set(operator.name, true);
-      }
-    });
-  }
-
-  if (product.prodCtx) {
-    const categoryString = `${product.addName || product.name} | ${
-      product.prodCtx.catCode
-    } | ${product.prodCtx.catOut} | ${product.prodCtx.catOutL} | ${
-      product.prodCtx.catOutS
-    }`;
-    void categoryCache.set(categoryString, true);
-  }
 
   return {
     name: product.addName || product.name,
