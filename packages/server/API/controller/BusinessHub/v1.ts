312e9381f90c17fb4dafc5ccd31652e4f8811d5d
marudor <marudor@marudor.de>
2020-12-07 13:48:25 +0100

    fix: husky dependency (#443)

--- packages/server/API/controller/BusinessHub/v1.ts
+++ packages/server/API/controller/BusinessHub/v1.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, OperationId, Route, Tags } from 'tsoa';
-import { stationOccupancy } from 'business-hub';
+import { fasta, stationOccupancy, stationQuays } from 'business-hub';
+import type { FastaResponse } from 'business-hub/types/Fasta';
 import type { OccupancyResponse } from 'business-hub/types/Occupancy';
+import type { Quay } from 'business-hub/types/Quays';
 
 @Route('/businessHub/v1')
 export class BusinessHubV1Controller extends Controller {
@@ -9,5 +11,19 @@ export class BusinessHubV1Controller extends Controller {
   @OperationId('Bahnhofs Auslastung v1')
   stationOccupancy(evaId: string, date: Date): Promise<OccupancyResponse> {
     return stationOccupancy(evaId, date);
+  }
+
+  @Get('/stationQuays/{evaId}')
+  @Tags('BusinessHub')
+  @OperationId('Gleise')
+  quays(evaId: string): Promise<Quay[]> {
+    return stationQuays(evaId);
+  }
+
+  @Get('/fasta/{stadaId}')
+  @Tags('BusinessHub')
+  @OperationId('Fahrstühle')
+  fasta(stadaId: string): Promise<FastaResponse> {
+    return fasta(stadaId);
   }
 }
