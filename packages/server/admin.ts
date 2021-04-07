9e2c90572f50bba95234afca84d301129a3263c3
marudor <marudor@marudor.de>
2021-04-07 17:34:20 +0200

    chore: update more deps

--- packages/server/admin.ts
+++ packages/server/admin.ts
@@ -1,18 +1,11 @@
-import Client from 'prom-client';
 import Koa from 'koa';
 import type { Server } from 'http';
 
 export default (adminPort = 9000): Server => {
-  Client.collectDefaultMetrics();
   const koa = new Koa();
 
   koa.use((ctx) => {
     switch (ctx.request.url) {
-      case '/metrics':
-        // eslint-disable-next-line no-console
-        console.log('SERVED METRICS');
-        ctx.body = Client.register.metrics();
-        break;
       case '/ping':
         ctx.body = 'pong';
         break;
