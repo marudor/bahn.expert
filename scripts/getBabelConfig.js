f469015ac4273779afae7fbea3e6926cacd02dca
marudor <marudor@marudor.de>
2021-04-07 17:23:34 +0200

    chore: update deps

--- scripts/getBabelConfig.js
+++ scripts/getBabelConfig.js
@@ -30,13 +30,9 @@ const getBabelConfig = (type) => {
       },
     ],
     '@loadable/babel-plugin',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
     'babel-plugin-parameter-decorator',
     './scripts/babelTransform/debugStyleNames.js',
-    'lodash',
     [
       'module-resolver',
       {
