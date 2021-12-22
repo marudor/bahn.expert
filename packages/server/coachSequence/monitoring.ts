0a9b81eeb74429c955e68a985064abe42d9d6cf8
marudor <marudor@marudor.de>
2021-12-22 10:55:16 +0100

    fix: hopefully no more crashes, also removed reihung v2 api (#552)

---
+++ packages/server/coachSequence/monitoring.ts
@@ -0,0 +1,48 @@
+import { coachSequence } from 'server/coachSequence';
+import { getAbfahrten } from 'server/iris';
+import type { CoachSequenceInformation } from 'types/coachSequence';
+
+function wagenReihungSpecificMonitoring(id: string, departure: Date) {
+  return coachSequence(id, departure);
+}
+
+export async function coachSequenceMonitoring(): Promise<
+  CoachSequenceInformation | undefined
+> {
+  const abfahrten = await getAbfahrten('8002549', false, {
+    lookahead: 300,
+  });
+  const maybeDepartures = abfahrten.departures.filter(
+    (d) => d.reihung && d.departure,
+  );
+
+  let departure = maybeDepartures.shift();
+
+  while (departure) {
+    const departureTime =
+      departure.departure && departure.departure.scheduledTime;
+
+    if (!departureTime) {
+      departure = maybeDepartures.shift();
+    } else {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const wr = await wagenReihungSpecificMonitoring(
+          departure.train.number,
+          departureTime,
+        );
+
+        if (wr) return wr;
+        departure = maybeDepartures.shift();
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        // console.warn(
+        //   'Failed to get WR for Monitoring!',
+        //   e,
+        //   departure && departure.train,
+        // );
+        departure = maybeDepartures.shift();
+      }
+    }
+  }
+}
