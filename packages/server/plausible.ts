76248de36126cba79e206763e21580d5343df721
marudor <marudor@marudor.de>
2022-02-10 15:31:42 +0100

    Revert "feat: track api usage on plausible"
    This reverts commit b67f321d63e250a5a28ec759e4e53dd7b90af220.

--- packages/server/plausible.ts
+++ packages/server/plausible.ts
@@ -1,24 +1,23 @@
 import Axios from 'axios';
-import type { Context, Request } from 'koa';
-import type { RouterParamContext } from '@koa/router';
+import type { Request } from 'koa';
 
-const plausibleUrl = process.env.PLAUSIBLE_URL;
-const environment = process.env.ENVIRONMENT;
+const url = process.env.PLAUSIBLE_URL;
+const domain = process.env.ENVIRONMENT;
 
 export async function deprecatedAPIUsage(
   req: Request,
   name: string,
 ): Promise<void> {
   try {
-    if (!plausibleUrl || !environment) {
+    if (!url || !domain) {
       return;
     }
     await Axios.post(
-      plausibleUrl,
+      url,
       {
         name,
         url: req.href,
-        domain: environment,
+        domain,
         referrer: req.header.referer,
         props: {
           userAgent: req.header['user-agent'],
@@ -36,50 +35,5 @@ export async function deprecatedAPIUsage(
     );
   } catch {
     //ignore errors
-  }
-}
-
-export async function apiUsage(
-  ctx: Context & RouterParamContext,
-): Promise<void> {
-  const route = ctx._matchedRoute?.toString();
-  const req = ctx.request;
-  const res = ctx.response;
-  try {
-    if (!plausibleUrl || !route || !environment) {
-      return;
-    }
-    const domain =
-      environment === 'production' ? 'marudor.de' : 'beta.marudor.de';
-    const isMarudorTraffic = req.header.referer?.startsWith(
-      `https://${domain}`,
-    );
-    const utmMedium = isMarudorTraffic ? 'marudor' : 'external';
-
-    const url = `${req.protocol}://${req.host}${route}?utm_source=api&utm_medium=${utmMedium}`;
-
-    await Axios.post(
-      plausibleUrl,
-      {
-        name: 'pageview',
-        url,
-        domain,
-        referrer: req.header.referer,
-        props: {
-          userAgent: req.header['user-agent'],
-          ip: req.header['x-real-ip'],
-          statusCode: res.status,
-        },
-      },
-      {
-        headers: {
-          'user-agent': req.header['user-agent']! || '',
-          'x-forwarded-for':
-            (req.header['x-real-ip'] as string) || req.ip || '',
-        },
-      },
-    );
-  } catch {
-    // ignore errors
   }
 }
