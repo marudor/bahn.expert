c69a03ff582965f08d4958edb52094a8719bef60
marudor <marudor@marudor.de>
2022-04-09 18:59:12 +0200

    fix: ICE3 RD should be done by now (#585)

--- packages/server/coachSequence/tzInfo.ts
+++
@@ -1,7 +0,0 @@
-import staticRedesign from './staticRedesign.json';
-
-export function isRedesignByTZ(tz?: string): boolean | undefined {
-  if (!tz) return;
-  const sanitizedTZ = tz.startsWith('TZ') ? tz.substr(2) : tz;
-  return staticRedesign.includes(sanitizedTZ);
-}
