e03c13afa07dea8ba927256be719ca7afcf9b7a6
marudor <marudor@marudor.de>
2022-04-15 17:00:05 +0200

    fix(routing): correctly sets time to now in "jetzt" mode (#588)

--- cypress/integration/routing/settings.test.ts
+++ cypress/integration/routing/settings.test.ts
@@ -1,4 +1,49 @@
 describe('Routing Settings', () => {
+  describe('mocked time', () => {
+    beforeEach(() => {
+      const initialDate = new Date('2022-04-14T13:00:00.000Z');
+      cy.clock(initialDate);
+      cy.visit('/routing');
+    });
+    it('sets to current Time on "jetzt"', () => {
+      cy.findByTestId('routingDatePicker').should(
+        'have.value',
+        'Jetzt (Heute 13:00)',
+      );
+      cy.tick(600 * 1000);
+      cy.findByTestId('search').click();
+      cy.findByTestId('routingDatePicker').should(
+        'have.value',
+        'Jetzt (Heute 13:10)',
+      );
+    });
+
+    it('Setting a time and resetting with "jetzt" resets untouched', () => {
+      cy.findByTestId('routingDatePicker').click();
+      cy.tick(600 * 1000);
+      cy.findByText('OK').click();
+      cy.findByTestId('routingDatePicker').should(
+        'have.value',
+        'Heute, Do 14.04. 13:00',
+      );
+      cy.findByTestId('search').click({ force: true });
+      cy.findByTestId('routingDatePicker')
+        .should('have.value', 'Heute, Do 14.04. 13:00')
+        .click({ force: true });
+      cy.findByText('Jetzt').click();
+      cy.findByTestId('routingDatePicker').should(
+        'have.value',
+        'Jetzt (Heute 13:10)',
+      );
+      cy.tick(600 * 1000);
+      cy.findByTestId('search').click();
+      cy.findByTestId('routingDatePicker').should(
+        'have.value',
+        'Jetzt (Heute 13:20)',
+      );
+    });
+  });
+
   describe('normal', () => {
     beforeEach(() => {
       cy.visit('/routing');
