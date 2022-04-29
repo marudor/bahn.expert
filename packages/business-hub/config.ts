601e602c0f595fbfaf8c6be6001cd99028e82455
marudor <marudor@marudor.de>
2022-04-29 15:10:04 +0200

    chore: use new apiKey and url for stations upstream (#589)

--- packages/business-hub/config.ts
+++ packages/business-hub/config.ts
@@ -4,8 +4,7 @@ export const risStationsConfiguration = new Configuration({
   apiKey:
     // Community key by Bahnhof Live App
     // https://github.com/dbbahnhoflive/dbbahnhoflive-android/blob/master/modules/core/build.gradle#L15
-    process.env.BUSINESS_HUB_STOP_PLACES_KEY ||
-    'TOL1jxXeqIW72s7vKPCcUuPNqFJTvPQx',
+    process.env.RIS_STATIONS_KEY || 'a5d3c62e032912922ed46cdacdb47940',
   baseOptions: {
     headers: {
       'user-agent': 'marudor.de',
