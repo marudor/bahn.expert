942536cda9e1bb9d5ceb957ca1059b22dd017ecd
marudor <marudor@marudor.de>
2022-04-30 13:42:02 +0200

    fix: actually transpile for node on all server packages

--- packages/business-hub/config.ts
+++ packages/business-hub/config.ts
@@ -5,6 +5,7 @@ export const risStationsConfiguration = new Configuration({
     // Community key by Bahnhof Live App
     // https://github.com/dbbahnhoflive/dbbahnhoflive-android/blob/master/modules/core/build.gradle#L15
     process.env.RIS_STATIONS_KEY || 'a5d3c62e032912922ed46cdacdb47940',
+  basePath: 'https://apis.deutschebahn.com/db/apis/ris-stations/v1',
   baseOptions: {
     headers: {
       'user-agent': 'marudor.de',
