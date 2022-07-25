110d6dfe3edaeec3967a34e050a3db39a51956d1
marudor <marudor@marudor.de>
2022-07-25 12:17:33 +0200

    fix: change iterableIsArray assumption as its not true

--- src/client/Abfahrten/Components/Abfahrt/Via/Detail.tsx
+++ src/client/Abfahrten/Components/Abfahrt/Via/Detail.tsx
@@ -16,7 +16,7 @@ export const DetailVia: FC<Props> = ({ stops }) => {
   const stopsToRender = useMemo(() => {
     const result: ReactNode[] = [];
 
-    stops.forEach((s, i) => {
+    for (const [i, s] of stops.entries()) {
       result.push(
         <StyledStationLink
           stop={s}
@@ -29,7 +29,7 @@ export const DetailVia: FC<Props> = ({ stops }) => {
       if (i + 1 !== stops.length) {
         result.push(' - ');
       }
-    });
+    }
 
     return result;
   }, [stops, urlPrefix]);
