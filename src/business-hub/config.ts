f5f3cd8504676fc81a988aa730cb0b6425de306d
marudor <marudor@marudor.de>
2022-08-16 22:15:44 +0200

    chore(ris-stations): change api authentication

--- src/business-hub/config.ts
+++ src/business-hub/config.ts
@@ -1,14 +1,13 @@
 import { Configuration } from 'business-hub/generated';
 
 export const risStationsConfiguration = new Configuration({
-  apiKey:
-    // Community key by Bahnhof Live App
-    // https://github.com/dbbahnhoflive/dbbahnhoflive-android/blob/master/modules/core/build.gradle#L15
-    process.env.RIS_STATIONS_KEY || 'a5d3c62e032912922ed46cdacdb47940',
-  basePath: 'https://apis.deutschebahn.com/db/apis/ris-stations/v1',
+  apiKey: process.env.RIS_STATIONS_KEY,
+  basePath: process.env.RIS_STATIONS_URL,
   baseOptions: {
     headers: {
       'user-agent': process.env.USER_AGENT || 'bahnhofs-abfahrten-default',
+      'DB-Api-Key': process.env.RIS_STATIONS_CLIENT_SECRET,
+      'DB-Client-Id': process.env.RIS_STATIONS_CLIENT_ID,
     },
   },
 });
