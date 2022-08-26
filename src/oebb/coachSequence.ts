eeb76b3f3f54c31ee4c0280d7499696862f06df3
marudor <marudor@marudor.de>
2022-08-26 13:51:02 +0200

    fix(coachSequence): oebb sequence fixed

--- src/oebb/coachSequence.ts
+++ src/oebb/coachSequence.ts
@@ -1,9 +1,15 @@
 import { format } from 'date-fns';
 import Axios from 'axios';
+import https from 'node:https';
 import type { OEBBInfo } from 'oebb/types/coachSequence';
 
 const client = Axios.create({
   baseURL: 'https://live.oebb.at/backend',
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+    // This allows connection to CVE-2009-3555 vulnerable servers. You shouldn't do that usually. idc here, I want coachSequences.
+    secureOptions: 0x4,
+  }),
 });
 
 export async function info(
