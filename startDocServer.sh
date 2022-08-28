526f667378c01786eef47d305f5f0280d653d4aa
marudor <marudor@marudor.de>
2022-08-28 18:41:41 +0200

    chore: dependencies (#661)

--- startDocServer.sh
+++ startDocServer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 echo "http://localhost:9043 for docs"
-nerdctl run -e SPEC_URL=/swagger.json -v "$(pwd)/public/swagger.json:/usr/share/nginx/html/swagger.json" -p 9043:80 redocly/redoc
+docker run -e SPEC_URL=/swagger.json -v "$(pwd)/public/swagger.json:/usr/share/nginx/html/swagger.json" -p 9043:80 redocly/redoc
