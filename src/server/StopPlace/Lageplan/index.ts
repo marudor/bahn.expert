38fb902265294fed1302fd0520d80dc82b71e5bb
marudor <marudor@marudor.de>
2022-09-24 19:08:09 +0200

    chore: remove broken HVV Lageplan stuff (#675)

--- src/server/StopPlace/Lageplan/index.ts
+++ src/server/StopPlace/Lageplan/index.ts
@@ -1,16 +1,14 @@
 import { getDBLageplan } from './DBLageplan';
-import { getHVVLageplan } from './HVVLageplan';
 import { getNAHSHLageplan } from './NAHSHLageplan';
 
 export async function getLageplan(
   stationName: string,
   evaId: string,
 ): Promise<string | undefined> {
-  const [DBLageplan, HVVLageplan, NahSHLageplan] = await Promise.all([
+  const [DBLageplan, NahSHLageplan] = await Promise.all([
     getDBLageplan(stationName),
-    getHVVLageplan(evaId),
     getNAHSHLageplan(evaId),
   ]);
 
-  return DBLageplan || HVVLageplan || NahSHLageplan;
+  return DBLageplan || NahSHLageplan;
 }
