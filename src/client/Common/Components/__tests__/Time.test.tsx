75ca2ef8081a452f12aef8878a33cd4aa39416a0
marudor <marudor@marudor.de>
2023-02-18 16:10:25 +0100

    fix: time tests

--- src/client/Common/Components/__tests__/Time.test.tsx
+++ src/client/Common/Components/__tests__/Time.test.tsx
@@ -14,86 +14,40 @@ describe('Time', () => {
   it('only scheduled Data', () => {
     render(<Time real={sampleTime} />);
 
-    expect(screen.queryByTestId('delay')).toBeNull();
-    expect(screen.queryByTestId('time')).toHaveTextContent('13:55');
+    expect(screen.queryByTestId('realTime')).toBeNull();
+    expect(screen.queryByTestId('scheduledTime')).toHaveTextContent('13:55');
   });
 
-  describe('showOriginalTime = false', () => {
-    it('10 Minutes delay', () => {
-      const { container, theme } = render(
-        <Time real={sampleTime} delay={10} />,
-      );
+  it('10 Minutes delay', () => {
+    const { container, theme } = render(<Time real={sampleTime} delay={10} />);
 
-      expect(screen.queryByTestId('time')).toHaveTextContent('13:55');
-      expect(screen.queryByTestId('delay')).toHaveTextContent('+10');
-      expect(container).toHaveStyle(`color: ${theme.colors.red}`);
-      expect(container).toMatchSnapshot();
-    });
-
-    it('5 Minutes early', () => {
-      const { container, theme } = render(
-        <Time real={sampleTime} delay={-5} />,
-      );
-
-      expect(screen.queryByTestId('time')).toHaveTextContent('13:55');
-      expect(screen.queryByTestId('delay')).toHaveTextContent('-5');
-      expect(container).toHaveStyle(`color: ${theme.colors.green}`);
-      expect(container).toMatchSnapshot();
-    });
+    expect(screen.queryByTestId('scheduledTime')).toHaveTextContent('13:45');
+    expect(screen.queryByTestId('realTime')).toHaveTextContent('13:55');
+    expect(screen.queryByTestId('realTime')).toHaveStyle(
+      `color: ${theme.colors.red}`,
+    );
+    expect(container).toMatchSnapshot();
   });
 
-  describe('showOriginalTime = true', () => {
-    it('10 Minutes delay', () => {
-      const { container, theme } = render(
-        <Time real={sampleTime} delay={10} />,
-        {
-          commonConfig: {
-            time: false,
-          },
-        },
-      );
+  it('5 Minutes early', () => {
+    const { container, theme } = render(<Time real={sampleTime} delay={-5} />);
 
-      expect(screen.queryByTestId('time')).toHaveTextContent('13:45');
-      expect(screen.queryByTestId('delay')).toHaveTextContent('+10');
-      expect(container).not.toHaveStyle(`color: ${theme.colors.red}`);
-      expect(container).toMatchSnapshot();
-    });
-
-    it('5 Minutes early', () => {
-      const { container, theme } = render(
-        <Time real={sampleTime} delay={-5} />,
-        {
-          commonConfig: {
-            time: false,
-          },
-        },
-      );
-
-      expect(screen.queryByTestId('time')).toHaveTextContent('14:00');
-      expect(screen.queryByTestId('delay')).toHaveTextContent('-5');
-      expect(container).not.toHaveStyle(`color: ${theme.colors.green}`);
-      expect(container).toMatchSnapshot();
-    });
+    expect(screen.queryByTestId('scheduledTime')).toHaveTextContent('14:00');
+    expect(screen.queryByTestId('realTime')).toHaveTextContent('13:55');
+    expect(screen.queryByTestId('realTime')).toHaveStyle(
+      `color: ${theme.colors.green}`,
+    );
+    expect(container).toMatchSnapshot();
   });
 
   it('shows 0 delay number', () => {
     const { container, theme } = render(<Time real={sampleTime} delay={0} />);
 
-    expect(screen.queryByTestId('time')).toHaveTextContent('13:55');
-    expect(screen.queryByTestId('delay')).toHaveTextContent('0');
-    expect(container).toHaveStyle(`color: ${theme.colors.green}`);
-    expect(container).toMatchSnapshot();
-  });
-
-  it('does not show 0 delay number', () => {
-    const { container, theme } = render(
-      <Time real={sampleTime} delay={0} showZero={false} />,
+    expect(screen.queryByTestId('scheduledTime')).toHaveTextContent('13:55');
+    expect(screen.queryByTestId('realTime')).toHaveTextContent('13:55');
+    expect(screen.queryByTestId('realTime')).toHaveStyle(
+      `color: ${theme.colors.green}`,
     );
-
-    expect(screen.queryByTestId('time')).toHaveTextContent('13:55');
-    expect(screen.queryByTestId('delay')).toBeNull();
-    expect(container).not.toHaveStyle(`color: ${theme.colors.green}`);
-    expect(container).not.toHaveStyle(`color: ${theme.colors.red}`);
     expect(container).toMatchSnapshot();
   });
 });
