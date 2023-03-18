09f8e7c954cab894df6853f42ecf28e526690c26
marudor <marudor@marudor.de>
2023-03-18 18:13:37 +0100

    fix: e2e tests for details, query intercept

--- cypress/e2e/details.test.ts
+++ cypress/e2e/details.test.ts
@@ -9,27 +9,44 @@ describe('Details', () => {
   });
 
   it('can render & header height correct', () => {
-    cy.intercept('/api/journeys/v1/details/S30665', {
-      fixture: 'details/S6',
-    }).as('details');
+    cy.intercept(
+      {
+        pathname: '/api/journeys/v1/details/S30665',
+      },
+      {
+        fixture: 'details/S6',
+      },
+    ).as('details');
     cy.visit('/details/S30665');
     cy.wait('@details');
-    // cy.percySnapshot('detailsHeader');
   });
 
   it('renders train line & number for regional stuff', () => {
-    cy.intercept('/api/journeys/v1/details/S30665', {
-      fixture: 'details/S6',
-    }).as('details');
+    cy.intercept(
+      {
+        pathname: '/api/journeys/v1/details/S30665',
+      },
+      {
+        fixture: 'details/S6',
+      },
+    ).as('details');
     cy.visit('/details/S30665');
     cy.wait('@details');
     cy.findByTestId('detailsTrainName').should('have.text', 'S 6 (30665)');
   });
 
   it('uses journeyId if provided', () => {
-    cy.intercept('/api/journeys/v1/details/S30665?journeyId=jid', {
-      fixture: 'details/S6',
-    }).as('details');
+    cy.intercept(
+      {
+        pathname: '/api/journeys/v1/details/S30665',
+        query: {
+          journeyId: 'jid',
+        },
+      },
+      {
+        fixture: 'details/S6',
+      },
+    ).as('details');
     cy.visit('/details/S30665?journeyId=jid');
     cy.wait('@details');
     cy.findByTestId('detailsTrainName').should('have.text', 'S 6 (30665)');
