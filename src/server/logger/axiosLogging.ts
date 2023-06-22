fe8e333c79a964d579fc06badae13237df007e65
marudor <marudor@marudor.de>
2023-06-22 20:13:57 +0200

    chore: enable axiosLogging on prod

--- src/server/logger/axiosLogging.ts
+++ src/server/logger/axiosLogging.ts
@@ -11,59 +11,57 @@ declare module 'axios' {
   }
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  if (!Array.isArray(Axios.defaults.transformRequest)) {
-    if (Axios.defaults.transformRequest) {
-      Axios.defaults.transformRequest = [Axios.defaults.transformRequest];
-    } else {
-      Axios.defaults.transformRequest = [];
-    }
+if (!Array.isArray(Axios.defaults.transformRequest)) {
+  if (Axios.defaults.transformRequest) {
+    Axios.defaults.transformRequest = [Axios.defaults.transformRequest];
+  } else {
+    Axios.defaults.transformRequest = [];
   }
-  Axios.defaults.transformRequest.push(function (data, _headers) {
-    let url = this.url;
-    if (url?.startsWith('/')) {
-      url = `${this.baseURL}${url}`;
-    }
-    if (!this.maru) {
-      this.maru = {
-        id: v4(),
-      };
-    }
-    logger.debug(
-      {
-        id: this.maru.id,
-        method: this.method?.toUpperCase(),
-        url,
-      },
-      'Request',
-    );
-    if (data) {
-      return data;
-    }
-  });
-
-  if (!Array.isArray(Axios.defaults.transformResponse)) {
-    if (Axios.defaults.transformResponse) {
-      Axios.defaults.transformResponse = [Axios.defaults.transformResponse];
-    } else {
-      Axios.defaults.transformResponse = [];
-    }
-  }
-
-  Axios.defaults.transformResponse.push(function (data, _header, status) {
-    let url = this.url;
-    if (url?.startsWith('/')) {
-      url = `${this.baseURL}${url}`;
-    }
-    const logFn = (status === 200 ? logger.debug : logger.warn).bind(logger);
-    logFn(
-      {
-        id: this.maru?.id || 'unknown',
-        method: this.method?.toUpperCase(),
-        url,
-      },
-      `${status} - Response`,
-    );
-    return data;
-  });
 }
+Axios.defaults.transformRequest.push(function (data, _headers) {
+  let url = this.url;
+  if (url?.startsWith('/')) {
+    url = `${this.baseURL}${url}`;
+  }
+  if (!this.maru) {
+    this.maru = {
+      id: v4(),
+    };
+  }
+  logger.debug(
+    {
+      id: this.maru.id,
+      method: this.method?.toUpperCase(),
+      url,
+    },
+    'Request',
+  );
+  if (data) {
+    return data;
+  }
+});
+
+if (!Array.isArray(Axios.defaults.transformResponse)) {
+  if (Axios.defaults.transformResponse) {
+    Axios.defaults.transformResponse = [Axios.defaults.transformResponse];
+  } else {
+    Axios.defaults.transformResponse = [];
+  }
+}
+
+Axios.defaults.transformResponse.push(function (data, _header, status) {
+  let url = this.url;
+  if (url?.startsWith('/')) {
+    url = `${this.baseURL}${url}`;
+  }
+  const logFn = (status === 200 ? logger.debug : logger.warn).bind(logger);
+  logFn(
+    {
+      id: this.maru?.id || 'unknown',
+      method: this.method?.toUpperCase(),
+      url,
+    },
+    `${status} - Response`,
+  );
+  return data;
+});
