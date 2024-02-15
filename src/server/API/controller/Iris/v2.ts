d5c20d180f715c47ec1a65ae1428d8b4844efe64
marudor <marudor@marudor.de>
2024-02-15 17:30:11 +0100

    fix: debug some performance stuff

--- src/server/API/controller/Iris/v2.ts
+++ src/server/API/controller/Iris/v2.ts
@@ -1,4 +1,4 @@
-import { addMinutes, isEqual, subMinutes } from 'date-fns';
+// import { addMinutes, isEqual, subMinutes } from 'date-fns';
 import {
   Controller,
   Get,
@@ -7,9 +7,9 @@ import {
   Route,
   Tags,
 } from '@tsoa/runtime';
-import { departureAndArrivals } from '@/external/risBoards';
+// import { departureAndArrivals } from '@/external/risBoards';
 import { getAbfahrten } from '@/server/iris';
-import { TimeType } from '@/external/generated/risBoards';
+// import { TimeType } from '@/external/generated/risBoards';
 import type { AbfahrtenResult } from '@/types/iris';
 import type { EvaNumber } from '@/types/common';
 
@@ -23,7 +23,7 @@ export class IrisControllerv2 extends Controller {
   @Get('/abfahrten/{evaNumber}')
   @Tags('IRIS')
   @OperationId('Abfahrten v2')
-  async abfahrten(
+  abfahrten(
     evaNumber: EvaNumber,
     /** minutes */
     @Query() lookahead = 150,
@@ -31,56 +31,61 @@ export class IrisControllerv2 extends Controller {
     @Query() lookbehind = 0,
     @Query() startTime?: Date,
   ): Promise<AbfahrtenResult> {
-    const defaultedStartTime = startTime || new Date();
-    const abfahrtenPromise = getAbfahrten(evaNumber, true, {
+    return getAbfahrten(evaNumber, true, {
       lookahead,
       lookbehind,
-      startTime: defaultedStartTime,
+      startTime,
     });
-    const boardsPromise = departureAndArrivals(
-      evaNumber,
-      subMinutes(defaultedStartTime, Math.min(60, lookbehind)),
-      addMinutes(defaultedStartTime, Math.min(120, lookahead)),
-    );
+    // const defaultedStartTime = startTime || new Date();
+    // const abfahrtenPromise = getAbfahrten(evaNumber, true, {
+    //   lookahead,
+    //   lookbehind,
+    //   startTime: defaultedStartTime,
+    // });
+    // const boardsPromise = departureAndArrivals(
+    //   evaNumber,
+    //   subMinutes(defaultedStartTime, Math.min(60, lookbehind)),
+    //   addMinutes(defaultedStartTime, Math.min(120, lookahead)),
+    // );
 
-    const abfahrten = await abfahrtenPromise;
-    try {
-      const board = await boardsPromise;
-      const allAbfahrten = [...abfahrten.departures, ...abfahrten.lookbehind];
-      for (const b of board) {
-        const abfahrt = allAbfahrten.find(
-          (a) => a.train.number === b.transport.number.toString(),
-        );
-        if (
-          abfahrt?.departure &&
-          b.departure &&
-          isEqual(
-            abfahrt.departure.scheduledTime,
-            new Date(b.departure.timeSchedule),
-          )
-        ) {
-          abfahrt.journeyId = b.departure.journeyID;
-          abfahrt.departure.isRealTime =
-            b.departure.timeType === TimeType.Real || undefined;
-        }
-        if (
-          abfahrt?.arrival &&
-          b.arrival &&
-          isEqual(
-            abfahrt.arrival.scheduledTime,
-            new Date(b.arrival.timeSchedule),
-          )
-        ) {
-          abfahrt.journeyId = b.arrival.journeyID;
-          abfahrt.arrival.isRealTime =
-            b.arrival.timeType === TimeType.Real || undefined;
-        }
-      }
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-    }
+    // const abfahrten = await abfahrtenPromise;
+    // try {
+    //   const board = await boardsPromise;
+    //   const allAbfahrten = [...abfahrten.departures, ...abfahrten.lookbehind];
+    //   for (const b of board) {
+    //     const abfahrt = allAbfahrten.find(
+    //       (a) => a.train.number === b.transport.number.toString(),
+    //     );
+    //     if (
+    //       abfahrt?.departure &&
+    //       b.departure &&
+    //       isEqual(
+    //         abfahrt.departure.scheduledTime,
+    //         new Date(b.departure.timeSchedule),
+    //       )
+    //     ) {
+    //       abfahrt.journeyId = b.departure.journeyID;
+    //       abfahrt.departure.isRealTime =
+    //         b.departure.timeType === TimeType.Real || undefined;
+    //     }
+    //     if (
+    //       abfahrt?.arrival &&
+    //       b.arrival &&
+    //       isEqual(
+    //         abfahrt.arrival.scheduledTime,
+    //         new Date(b.arrival.timeSchedule),
+    //       )
+    //     ) {
+    //       abfahrt.journeyId = b.arrival.journeyID;
+    //       abfahrt.arrival.isRealTime =
+    //         b.arrival.timeType === TimeType.Real || undefined;
+    //     }
+    //   }
+    // } catch (e) {
+    //   // eslint-disable-next-line no-console
+    //   console.error(e);
+    // }
 
-    return abfahrten;
+    // return abfahrten;
   }
 }
