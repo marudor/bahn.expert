0e10db5775a47e1a615c889a9b25b48fcd5ab78a
marudor <marudor@marudor.de>
2024-06-10 19:14:28 +0200

    fix: typo politic

--- src/client/PolitikPopup.tsx
+++ src/client/PolitikPopup.tsx
@@ -21,25 +21,26 @@ export const PolitikPopup: FC = () => {
       <DialogContent>
         Wir haben einen Tag nach der Europawahl und ich bin sauer und traurig...
         <br />
-        Hiermit in alle AFD Wählenden: <strong>FICKT EUCH!</strong>
+        Hiermit an alle AFD Wählenden: <strong>FICKT EUCH!</strong>
         <br />
-        Ihr seit die mit Abstand größte Gefahr für unsere Gesellschaft! Verpisst
+        Ihr seid die mit Abstand größte Gefahr für unsere Gesellschaft! Verpisst
         euch einfach von bahn.expert - wenn ich könnte würde ich technisch
-        verhindern das ihr hier seit!
+        verhindern das ihr hier seid!
+        <br />
+        Hier und auch nirgends anders will man euch haben. Euer Gedankengut ist
+        widerlich, Menschenverachtend und verursacht Brechreiz.
         <br />
         Das Projekt hier soll eigentlich nur eure Bahnfahrten erleichtern. Aber
         in Zeiten wo eine Partei welche in Teilen gesichert rechtsextrem ist und
-        überall ein rechtsextremer Verdachtsfall ist. Und das von unserem
-        Verfassungschutz welcher auf dem rechten Auge leider viele Dioptrien
-        besitzt...
+        überall ein rechtsextremer Verdachtsfall ist muss ich leider Politik
+        einbringen.
         <br />
-        An alle Menschen die sich gegen rechtsextremismus und fachismus
+        An alle Menschen die sich gegen Rechtsextremismus und Fachismus
         einsetzen: Danke euch!
         <br />
-        An alle Menschen die eher konservativ sind - aber nicht rechtsextrem
-        sind Also Mindestens CDU Wählende zum Beispiel. Wir mögen politisch
-        nicht zueinander finden. Aber immerhin fallt ihr nicht auf die Lügen der
-        AFD rein!
+        An alle Menschen die eher konservativ sind, aber nicht rechtsextrem
+        sind, also Mindestens CDU Wählende. Wir mögen politisch nicht zueinander
+        finden. Aber immerhin fallt ihr nicht auf die Lügen der AFD rein!
         <br />
         Das reicht dann auch, so lange ihr eure Settings nicht löscht seht ihr
         das hier nicht wieder.
