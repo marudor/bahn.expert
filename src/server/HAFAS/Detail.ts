607d12c0e4efbf37ef15a942d4fd423878b9b578
marudor <marudor@marudor.de>
2024-07-27 10:49:57 +0200

    chore: devDependencies

--- src/server/HAFAS/Detail.ts
+++ src/server/HAFAS/Detail.ts
@@ -8,7 +8,7 @@ import searchOnTrip from './SearchOnTrip';
 import type { JourneyFilter } from '@/types/HAFAS';
 import type { ParsedJourneyMatchResponse } from '@/types/HAFAS/JourneyMatch';
 import type { ParsedSearchOnTripResponse } from '@/types/HAFAS/SearchOnTrip';
-import type { RouteJourneySegmentTrain, RouteStop } from '@/types/routing';
+import type { RouteStop } from '@/types/routing';
 
 export function calculateCurrentStopPlace(
   segment: ParsedSearchOnTripResponse,
@@ -146,9 +146,7 @@ export default async (
       hafasProfile,
     );
 
-    relevantSegment = route.segments.find(
-      (s) => s.type === 'JNY',
-    ) as RouteJourneySegmentTrain;
+    relevantSegment = route.segments.find((s) => s.type === 'JNY')!;
   } catch {
     // we keep using the JourneyDetailsOne
   }
