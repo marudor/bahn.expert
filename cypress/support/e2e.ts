4355619cc301954a84f849940c5daede3141f226
marudor <marudor@marudor.de>
2025-02-02 20:37:20 +0100

    fix(workaround): skip tests that need SSR Mocking

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -21,8 +21,8 @@ cy.visit = (...args) => {
 };
 
 beforeEach(() => {
-	cy.force404();
 	cy.setCookie('timesPoliticSeen2', '200');
 	cy.setCookie('timesFeedback', '200');
 	cy.setCookie('bw2025', '200');
+	cy.force404();
 });
